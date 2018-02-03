d7dd8292943afa269a4aaada2f9ce1af4208d87a
Justin Fear <justin.m.fear@gmail.com>
2018-02-03 16:22:18 -0500

    Adds config import.
    I found that having access to the common config is useful outside of the
    a notebook situation. I moved this to a separate module for easy import.

--- lib/larval_gonad/io.py
+++ lib/larval_gonad/io.py
@@ -1,8 +1,14 @@
 """Collection of io related items."""
 from pathlib import Path
 from joblib import Memory
+from yaml import load
 
+# Create Cache
 cache_dir = Path(Path(__file__).parent, '../../output/cache').resolve()
 cache_dir.mkdir(exist_ok=True)
-
 memory = Memory(cachedir=cache_dir, verbose=0)
+
+# Get config
+cname = Path(Path(__file__).parent, '../../config/common.yml').resolve()
+with cname.open() as fh:
+    config = load(fh)
