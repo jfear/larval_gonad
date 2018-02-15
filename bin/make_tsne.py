206a66fde292925c59a3e5d8c67506a7aa1536a5
Justin Fear <justin.m.fear@gmail.com>
2018-02-15 11:14:10 -0500

    Adds new replicates.
    Adds new replicates for testis and ovary.

--- bin/make_tsne.py
+++ bin/make_tsne.py
@@ -18,9 +18,11 @@ from larval_gonad.logging import logger
 REF = os.environ['REFERENCES_DIR']
 add_styles('../config/stylelib')
 
+
 def sanitize_fname(fname):
     valid_chars = "-_.%s%s" % (string.ascii_letters, string.digits)
     return ''.join([x for x in fname if x in valid_chars])
+
 
 def plot_gene(data, fbgn, symbol, output, **kwargs):
     symbol = sanitize_fname(symbol)
@@ -31,7 +33,8 @@ def plot_gene(data, fbgn, symbol, output, **kwargs):
     df = data[['tSNE_1', 'tSNE_2', fbgn]]
 
     with plt.style.context(['paper-wide', 'default']):
-        fig, (ax1, ax2) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [1.3, 1]})
+        fig, (ax1, ax2) = plt.subplots(1, 2,
+                                       gridspec_kw={'width_ratios': [1.3, 1]})
         TSNEPlot('tSNE_2', 'tSNE_1', data=df, hue=fbgn, s=10,
                  ax=ax1, title='Normalized Expression\n(Continuous)', **kwargs)
 
@@ -39,29 +42,104 @@ def plot_gene(data, fbgn, symbol, output, **kwargs):
                  cmap={
                      '0': 'w',
                      '1': 'k',
-                 }, s=10, ax=ax2, alpha=.6, edgecolor='k', title='Normalized Expression\n(Binary)', **kwargs)
+                 }, s=10, ax=ax2, alpha=.6, edgecolor='k',
+                 title='Normalized Expression\n(Binary)', **kwargs)
 
-        fig.suptitle(f'{symbol} ({fbgn})');
+        fig.suptitle(f'{symbol} ({fbgn})')
         plt.tight_layout(rect=[0, 0, .9, .9])
         plt.savefig(fname)
         plt.close()
 
+
 if __name__ == '__main__':
     # gene annotations
-    fbgn2symbol = pd.read_csv(str(Path(REF, 'dmel/r6-16/fb_annotation/dmel_r6-16.fb_annotation')), sep='\t',
-                              usecols=['gene_symbol', 'primary_FBgn'], index_col='primary_FBgn').to_dict()['gene_symbol']
+    fbgn2symbol = pd.read_csv(
+        str(Path(REF, 'dmel/r6-16/fb_annotation/dmel_r6-16.fb_annotation')),
+        sep='\t', usecols=['gene_symbol', 'primary_FBgn'],
+        index_col='primary_FBgn'
+    ).to_dict()['gene_symbol']
 
     # Colors
     colors = sns.color_palette('Reds')
     color2 = sns.color_palette('Greys')
     colors[0] = color2[0]
 
-    # testes
-    logger.info('Plotting Testes')
-    FIGS = '../output/figures/testis_tsne'
+    # testes1
+    logger.info('Plotting Testes Rep 1')
+    FIGS = '../output/figures/testis1_tsne'
     Path(FIGS).mkdir(exist_ok=True)
 
-    DAT = '../output/testes_scRNAseq_pilot'
+    DAT = '../output/testes1_scRNAseq_pilot'
+    tsne = pd.read_csv(Path(DAT, 'tsne.tsv'), sep='\t')
+    norm = pd.read_csv(Path(DAT, 'normalized_read_counts.tsv'), sep='\t')
+    data = tsne.join(norm.T)
+
+    for fbgn in data.columns[2:]:
+        symbol = fbgn2symbol[fbgn]
+        plot_gene(data, fbgn, symbol, FIGS, palette=colors)
+
+    # testes2
+    logger.info('Plotting Testes Rep 2')
+    FIGS = '../output/figures/testis2_tsne'
+    Path(FIGS).mkdir(exist_ok=True)
+
+    DAT = '../output/testes2_scRNAseq_pilot'
+    tsne = pd.read_csv(Path(DAT, 'tsne.tsv'), sep='\t')
+    norm = pd.read_csv(Path(DAT, 'normalized_read_counts.tsv'), sep='\t')
+    data = tsne.join(norm.T)
+
+    for fbgn in data.columns[2:]:
+        symbol = fbgn2symbol[fbgn]
+        plot_gene(data, fbgn, symbol, FIGS, palette=colors)
+
+    # testes3
+    logger.info('Plotting Testes Rep 3')
+    FIGS = '../output/figures/testis3_tsne'
+    Path(FIGS).mkdir(exist_ok=True)
+
+    DAT = '../output/testes3_scRNAseq_pilot'
+    tsne = pd.read_csv(Path(DAT, 'tsne.tsv'), sep='\t')
+    norm = pd.read_csv(Path(DAT, 'normalized_read_counts.tsv'), sep='\t')
+    data = tsne.join(norm.T)
+
+    for fbgn in data.columns[2:]:
+        symbol = fbgn2symbol[fbgn]
+        plot_gene(data, fbgn, symbol, FIGS, palette=colors)
+
+    # ovary1
+    logger.info('Plotting Ovary Rep 1')
+    FIGS = '../output/figures/ovary1_tsne'
+    Path(FIGS).mkdir(exist_ok=True)
+
+    DAT = '../output/ovary1_scRNAseq_pilot'
+    tsne = pd.read_csv(Path(DAT, 'tsne.tsv'), sep='\t')
+    norm = pd.read_csv(Path(DAT, 'normalized_read_counts.tsv'), sep='\t')
+    data = tsne.join(norm.T)
+
+    for fbgn in data.columns[2:]:
+        symbol = fbgn2symbol[fbgn]
+        plot_gene(data, fbgn, symbol, FIGS, palette=colors)
+
+    # ovary2
+    logger.info('Plotting Ovary Rep 2')
+    FIGS = '../output/figures/ovary2_tsne'
+    Path(FIGS).mkdir(exist_ok=True)
+
+    DAT = '../output/ovary2_scRNAseq_pilot'
+    tsne = pd.read_csv(Path(DAT, 'tsne.tsv'), sep='\t')
+    norm = pd.read_csv(Path(DAT, 'normalized_read_counts.tsv'), sep='\t')
+    data = tsne.join(norm.T)
+
+    for fbgn in data.columns[2:]:
+        symbol = fbgn2symbol[fbgn]
+        plot_gene(data, fbgn, symbol, FIGS, palette=colors)
+
+    # ovary3
+    logger.info('Plotting Ovary Rep 3')
+    FIGS = '../output/figures/ovary3_tsne'
+    Path(FIGS).mkdir(exist_ok=True)
+
+    DAT = '../output/ovary3_scRNAseq_pilot'
     tsne = pd.read_csv(Path(DAT, 'tsne.tsv'), sep='\t')
     norm = pd.read_csv(Path(DAT, 'normalized_read_counts.tsv'), sep='\t')
     data = tsne.join(norm.T)
@@ -76,20 +154,6 @@ if __name__ == '__main__':
     Path(FIGS).mkdir(exist_ok=True)
 
     DAT = '../output/combined_ovary_and_testis_scRNAseq_pilot'
-    tsne = pd.read_csv(Path(DAT, 'tsne.tsv'), sep='\t')
-    norm = pd.read_csv(Path(DAT, 'normalized_read_counts.tsv'), sep='\t')
-    data = tsne.join(norm.T)
-
-    for fbgn in data.columns[2:]:
-        symbol = fbgn2symbol[fbgn]
-        plot_gene(data, fbgn, symbol, FIGS, palette=colors)
-
-    # ovary
-    logger.info('Plotting Ovary')
-    FIGS = '../output/figures/ovary_tsne'
-    Path(FIGS).mkdir(exist_ok=True)
-
-    DAT = '../output/ovary_scRNAseq_pilot'
     tsne = pd.read_csv(Path(DAT, 'tsne.tsv'), sep='\t')
     norm = pd.read_csv(Path(DAT, 'normalized_read_counts.tsv'), sep='\t')
     data = tsne.join(norm.T)
