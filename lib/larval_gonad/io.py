c08af6a646e691e17b6633097ebb3f2fc1ba0ce4
Justin Fear <justin.m.fear@gmail.com>
2018-03-20 09:11:01 -0400

    Adds code to encode and decode sequences.
    10x genomics stores their barcodes as 2-bit encoded numbers. I need to
    decode these so I copied some functions over to do this encoding and
    decoding.

--- lib/larval_gonad/io.py
+++ lib/larval_gonad/io.py
@@ -1,7 +1,11 @@
 """Collection of io related items."""
+import numpy as np
 import pandas as pd
 import scipy.sparse as sp_sparse
 import tables
+
+NUCS = ['A', 'C', 'G', 'T']
+NUCS_INVERSE = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
 
 
 # Import Cell Ranger molecular info
@@ -30,3 +34,53 @@ def cellranger_counts(fname, genome='dm6.16'):
         index=[x.decode() for x in gene_ids],
         columns=[x.decode() for x in barcodes]
     )
+
+
+def compress_seq(s: str):
+    """ Pack a DNA sequence (no Ns!) into a 2-bit format, in a 64-bit uint
+
+    Most significant bit is set if there was an error
+
+    Based on code from: https://github.com/10XGenomics/cellranger
+
+    cellranger/lib/python/cellranger/utils.py
+
+    """
+    bits = 64
+    assert len(s) <= (bits/2 - 1)
+    result = 0
+    for nuc in s:
+        if nuc not in NUCS_INVERSE:
+            return 1 << (bits - 1)
+        result = result << 2
+        result = result | NUCS_INVERSE[nuc]
+    return result
+
+
+def decompress_seq(x: int, length=16):
+    """ Un-pack a DNA sequence from a 2-bit format
+
+    Based on code from: https://github.com/10XGenomics/cellranger
+
+    cellranger/lib/python/cellranger/utils.py
+
+    Parameters
+    ----------
+    x : int
+        Number sequence to be decoded.
+    length : int
+        Length of the barcode. This can be found in the molecular info hdf5
+        file from 10x genome.
+        molInfo.get_node_attr('/metrics', 'chemistry_barcode_read_length')
+
+    """
+    bits = 64
+    x = np.uint64(x)
+    assert length <= (bits/2 - 1)
+    if x & (1 << (bits-1)):
+        return 'N' * length
+    result = bytearray(length)
+    for i in range(length):
+        result[(length-1)-i] = bytearray(NUCS[x & np.uint64(0b11)].encode())[0]
+        x = x >> np.uint64(2)
+    return str(result)
