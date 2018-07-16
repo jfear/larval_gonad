0ba9ebff600563f11de08308e27b0d379f131f0d
Justin Fear <justin.m.fear@gmail.com>
2018-07-16 09:13:12 -0400

    Adds a helper to play a sound when notebook done running.

---
+++ larval_gonad/sounds.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Make a beep in the notebook
+# >>> from IPython.display import Audio
+# >>> Audio(**beep)
+beep = {
+    'data': np.sin(2*np.pi*400*np.arange(10000*2)/20000),
+    'rate': 20000,
+    'autoplay': True
+}
