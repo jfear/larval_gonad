5f5158c9929af072f6a9f8734e7ee3cf70d2bf6e
Justin Fear <justin.m.fear@gmail.com>
2018-11-08 17:35:05 -0500

    Adds building results table back to the pipeline.

--- science_submission/scripts/table_scRNASeq_cluster_deg.py
+++ science_submission/scripts/table_scRNASeq_cluster_deg.py
@@ -1,23 +1,55 @@
+from textwrap import dedent
+
 import pandas as pd
 
+from larval_gonad.config import config
 
-def _add_sheet(writer : pd.ExcelWriter, sheet_name : str, fname : str, alpha=0.01, comment=None, headers=None):
+from common import fbgn2symbol
+
+def _add_sheet(writer : pd.ExcelWriter,
+               sheet_name : str,
+               fname : str,
+               cell_format,
+               alpha=0.01,
+               comment=None,
+               headers=None):
+
     sheet = writer.book.add_worksheet(sheet_name)
     writer.sheets[sheet_name] = sheet
     sheet.set_column(0, 1, 20)
 
-    df = pd.read_csv(Path(seurat_dir, fname), sep='\t').query(f'p_val_adj <= {alpha}')
+    df = pd.read_csv(fname, sep='\t').query(f'p_val_adj <= {alpha}')
     df.sort_values(by='avg_logFC', ascending=False, inplace=True)
 
     if 'cluster' in df.columns:
         df.sort_values(by='cluster', inplace=True)
-        df.cluster.replace(CLUSTER_ANNOT, inplace=True)
+        df.cluster.replace(config['cluster_annot'], inplace=True)
         idx = df.columns.tolist().index('cluster')
         sheet.set_column(idx, idx, 20)
 
     if headers:
         df.rename({'pct.1': headers[0], 'pct.2': headers[1]}, inplace=True, axis=1)
     df.to_excel(writer, sheet_name=sheet_name, index=False, startrow=1, freeze_panes=(2, 2))
+
+    if comment:
+        sheet.set_row(0, 100, cell_format)
+        sheet.merge_range('A1:G1', dedent(comment))
+
+
+def _add_data(writer : pd.ExcelWriter,
+               sheet_name : str,
+               fname : str,
+               cell_format,
+               comment=None):
+
+    sheet = writer.book.add_worksheet(sheet_name)
+    writer.sheets[sheet_name] = sheet
+    sheet.set_column(0, 1, 20)
+
+    df = pd.read_parquet(fname)[config['sel_cluster_order_w_rep']]
+    df['gene'] = df.index.map(lambda x: fbgn2symbol[x])
+    df.set_index('gene', append=True, inplace=True)
+    df.to_excel(writer, sheet_name=sheet_name, index=True, startrow=1, freeze_panes=(2, 2))
 
     if comment:
         sheet.set_row(0, 100, cell_format)
@@ -34,82 +66,60 @@ def main():
               "This creates a list of genes that are up-regulated in each cluster. " \
               "This table is grouped by clusters and sorted by avg_logFC."
 
-    _add_sheet(writer, 'One vs Rest (biomarkers)', f'biomarkers_{resolution}.tsv', comment=comment)
+    _add_sheet(writer, 'One vs Rest (biomarkers)', snakemake.input.biomarkers, cell_format, comment=comment)
 
     comment = "Differential expression between germ cell and somatic cell clusters. " \
-              "For this analysis I combine all of the germ cell clusters (6, 3, 2, 0) vs all of the somatic cell "\
+              "I combine all of the germ cell clusters (6, 3, 2, 0) vs all of the somatic cell "\
               "clusters (5, 1, 4, 7, 8).\n" \
               "Positive avg_logFC are germ biased genes.\n" \
               "Negative avg_logFC are soma biased genes.\n"
 
-    _add_sheet(writer, 'Germ Cells vs Somatic Cells', '2018-05-16_scrnaseq_germ_vs_soma_biomarkers.tsv',
-               comment=comment, headers=('pct.germ', 'pct.soma'))
+    _add_sheet(writer, 'Germ Cells vs Somatic Cells', snakemake.input.germ_soma, cell_format, comment=comment,
+               headers=('pct.germ', 'pct.soma'))
 
-    comment = "Here I have done a differential expression of spermatogonia vs 1º spermatocytes. " \
-              "For this analysis I took the spermatogonia cluster and compared it to all spermatocyte " \
-              "clusters combined together.\n\n" \
+    comment = "Differential expression of spermatogonia vs 1º spermatocytes. " \
+              "Spermatogonia cluster and compared it to all spermatocyte clusters combined together.\n\n" \
               "Positve avg_logFC are spermatogonia biased genes.\n" \
               "Negative avg_logFC are 1º spermatocyte biased genes."
 
-    _add_sheet(
-        writer,
-        'Gonia vs Cytes',
-        '2018-05-16_scrnaseq_spermatogonia_vs_spermatocytes_biomarkers.tsv',
-        comment=comment,
-        headers=('pct.gonia', 'pct.cytes')
-    )
+    _add_sheet(writer, 'Gonia vs Cytes', snakemake.input.gonia_cytes, cell_format, comment=comment,
+               headers=('pct.gonia', 'pct.cytes'))
 
-    comment = "Here I have done a differential expression of Early 1º spermatocytes vs Mid and Late 1º spermatocytes.\n" \
+    comment = "Differential expression of Early 1º spermatocytes vs Mid and Late 1º spermatocytes.\n" \
               "Positve avg_logFC are early 1º spermatocyte biased genes.\n" \
               "Negative avg_logFC are mid and late 1º spermatocyte biased genes."
 
-    _add_sheet(
-        writer,
-        'Early cytes vs Mid and Late',
-        '2018-05-16_scrnaseq_early_spermatocytes_vs_spermatocytes_biomarkers.tsv',
-        comment=comment,
-        headers=('pct.early', 'pct.midLate')
-    )
+    _add_sheet(writer, 'Early cytes vs Mid and Late', snakemake.input.gonia_early, cell_format, comment=comment,
+               headers=('pct.early', 'pct.midLate'))
 
-    comment = "Here I have done a differential expression of Mid 1º spermatocytes vs Early and Late 1º spermatocytes.\n" \
+    comment = "Differential expression of Mid 1º spermatocytes vs Early and Late 1º spermatocytes.\n" \
               "Positve avg_logFC are mid 1º spermatocyte biased genes.\n" \
               "Negative avg_logFC are early and late 1º spermatocyte biased genes."
 
-    _add_sheet(
-        writer,
-        'Mid cytes vs Early and Late',
-        '2018-05-16_scrnaseq_mid_spermatocytes_vs_spermatocytes_biomarkers.tsv',
-        comment=comment,
-        headers=('pct.mid', 'pct.earlyLate')
-    )
+    _add_sheet(writer, 'Mid cytes vs Early and Late', snakemake.input.gonia_mid, cell_format, comment=comment,
+               headers=('pct.mid', 'pct.earlyLate'))
 
-    comment = "Here I have done a differential expression of Late 1º spermatocytes vs Early and Mid 1º spermatocytes.\n" \
+    comment = "Differential expression of Late 1º spermatocytes vs Early and Mid 1º spermatocytes.\n" \
               "Positve avg_logFC are late 1º spermatocyte biased genes.\n" \
               "Negative avg_logFC are early and mid 1º spermatocyte biased genes."
 
-    _add_sheet(
-        writer,
-        'Late cytes vs Early and Mid',
-        '2018-05-16_scrnaseq_late_spermatocytes_vs_spermatocytes_biomarkers.tsv',
-        comment=comment,
-        headers=('pct.late', 'pct.earlyMid')
-    )
+    _add_sheet(writer, 'Late cytes vs Early and Mid', snakemake.input.gonia_late, cell_format, comment=comment,
+               headers=('pct.late', 'pct.earlyMid'))
 
-    comment = "Cluster eleven is a little bit of a mystery to us. " \
-              "It behaves kind of like a 1º spermatocyte, but has very low expression. " \
-              "Here I run a differential expression between cluster eleven and the 1º spermatocyte clusters.\n" \
-              "Positve avg_logFC are cluster 11 biased genes.\n" \
-              "Negative avg_logFC are 1º spermatocyte biased genes."
+    comment = "These are raw counts aggregated by Cluster:Rep using Sum."
 
-    _add_sheet(
-        writer,
-        'Cluster 11 vs cytes',
-        '2018-05-16_scrnaseq_eleven_vs_spermatocytes_biomarkers.tsv',
-        comment=comment,
-        headers=('pct.eleven', 'pct.cytes')
-    )
+    _add_data(writer, 'Raw Counts (Sum)', snakemake.input.raw, cell_format, comment=comment)
+
+    comment = "These are tpm normalized counts by Cluster:Rep."
+
+    _add_data(writer, 'TPM', snakemake.input.tpm, cell_format, comment=comment)
+
+    comment = "These are z-scores of tpm normalized counts by Cluster:Rep."
+
+    _add_data(writer, 'Z-scores', snakemake.input.zscore, cell_format, comment=comment)
 
     writer.close()
+
 
 if __name__ == '__main__':
     main()
