437d8f6caff20116611939055e0fd466a5a7a3c7
Justin Fear <justin.m.fear@gmail.com>
2018-11-08 17:34:45 -0500

    Removes svgutils since it is no longer used.

--- science_submission/scripts/common/__init__.py
+++ science_submission/scripts/common/__init__.py
@@ -4,15 +4,10 @@ from pathlib import Path
 from pickle import load
 
 import matplotlib as mpl
-from svgutils.compose import Text
 import pandas as pd
 
 from larval_gonad.config import CONFIG_DIR
 from larval_gonad.plotting import add_styles
-
-
-def label(text):
-    return Text(text, 5, 10, size=12, weight="bold", font="sans-serif")
 
 
 # Setup general plotting info
