56cf9a6e9e8155af03c1a108a003cd5e2e4aea88
Justin M. Fear <justin.m.fear@gmail.com>
2019-03-05 13:58:41 -0500

    Clean up

--- science_submission/scripts/parse_fbgn2chrom.py
+++ science_submission/scripts/parse_fbgn2chrom.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """Quick script to make a table with FBgn to chromosome."""
-
 import pandas as pd
-
 
 gtf = snakemake.input[0]
 oname = snakemake.output[0]
+CHROMS = ['chr2L', 'chr2R', 'chr3L', 'chr3R', 'chr4', 'chrX', 'chrY', 'chrM']
 
 genes = []
 with open(gtf) as fh:
@@ -18,10 +17,10 @@ with open(gtf) as fh:
         if rows[2] == 'gene':
             genes.append((rows[0], rows[9].replace('"', '').replace(';', '')))
 
-fbgn2chrom = pd.DataFrame(genes, columns=['chrom', 'FBgn'])
-fbgn2chrom.set_index('FBgn', inplace=True)
 
-CHROMS = ['chr2L', 'chr2R', 'chr3L', 'chr3R', 'chr4', 'chrX', 'chrY', 'chrM']
-fbgn2chrom = fbgn2chrom[fbgn2chrom['chrom'].isin(CHROMS)]
-
-fbgn2chrom.to_parquet(oname)
+(
+    pd.DataFrame(genes, columns=['chrom', 'FBgn'])
+    .set_index('FBgn')
+    .query(f'chrom == {CHROMS}')
+    .to_parquet(oname)
+)
