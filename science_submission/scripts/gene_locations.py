0b8d6bb54bce20d4db17c5cec457543b97c4e4c8
Justin M. Fear <justin.m.fear@gmail.com>
2019-03-05 14:13:48 -0500

    Clean up.

--- science_submission/scripts/gene_locations.py
+++ science_submission/scripts/gene_locations.py
@@ -1,26 +1,31 @@
 """Create a table of gene locations."""
-from pathlib import Path
-
 import numpy as np
 import pandas as pd
 import gffutils
 
 from larval_gonad.config import config
 
-db = gffutils.FeatureDB(snakemake.input[0])
+fname = snakemake.input[0]
+oname = snakemake.output[0]
+chrom_order = snakemake.params.chrom_order
 
-# organize genes by chrom position
+
+# Parse GTF to get gene and chrom position
+db = gffutils.FeatureDB(fname)
 locs = []
 for gene in db.features_of_type('gene'):
     fbgn = gene.id
     chrom = gene.chrom
     pos = np.min([gene.start, gene.end])
     locs.append((fbgn, chrom, pos))
-locs = pd.DataFrame(locs, columns=['FBgn', 'chrom', 'pos']).set_index('FBgn')
-locs.chrom = pd.Categorical(locs.chrom, categories=config['chrom_order'], ordered=True)
 
-# sort and make an arbitrary location point
-locs.sort_values(['chrom', 'pos'], inplace=True)
-locs['location'] = range(1, locs['pos'].shape[0] + 1)
-
-locs.to_parquet(snakemake.output[0])
+# Order by chrom and position
+(
+    pd.DataFrame(locs, columns=['FBgn', 'chrom', 'pos'])
+    .set_index('FBgn')
+    .query(f'chrom == {chrom_order}')
+    .assign(chrom=lambda df: df.chrom.astype('category').cat.as_ordered().cat.reorder_categories(chrom_order))
+    .sort_values(['chrom', 'pos'])
+    .assign(location=lambda df: range(1, df.shape[0] + 1))
+    .to_parquet(oname)
+)
