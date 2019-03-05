34e51febc391b67938f79dc782e42b88bd60dc59
Justin M. Fear <justin.m.fear@gmail.com>
2019-03-05 13:34:17 -0500

    Simplified cluster data file.

--- science_submission/scripts/parse_clusters.py
+++ science_submission/scripts/parse_clusters.py
@@ -5,20 +5,20 @@ clustering settings and method. Here I pull out the clusters based on the
 provided resolution. I then add on the cluster annotations for easier reading.
 
 """
-
 import pandas as pd
 
-from larval_gonad.config import config
-
 fname = snakemake.input[0]
-res = snakemake.params.resolution
+resolution = snakemake.params.resolution
+annotation = snakemake.params.annotation
 oname = snakemake.output[0]
 
-# import cluster calls for specific resolution
-sr = pd.read_csv(fname, sep='\t')[res]
-sr.name = 'cluster_num'
-df = sr.to_frame()
+df = (
+    pd.read_csv(fname, sep='\t')[resolution]
+    .rename('cluster')
+    .map(lambda x: annotation[x])
+    .to_frame()
+)
 
-# Add on cluster annotations for easier understanding
-df['cluster_name'] = df['cluster_num'].map(config['cluster_annot'])
+df.index.name = 'cell_id'
 df.to_parquet(oname)
+
