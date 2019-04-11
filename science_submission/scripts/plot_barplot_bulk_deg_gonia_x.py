f86341ccf88e7d2fccf721c93fbf1fd97eee8ef6
Justin M. Fear <justin.m.fear@gmail.com>
2019-04-11 13:23:26 -0400

    Change bulk vs gonia demasculinization analysis to use all genes.
    Maria said we should use all genes for the analysis because that is what
    people do in the literature.

--- science_submission/scripts/plot_barplot_bulk_deg_gonia_x.py
+++ science_submission/scripts/plot_barplot_bulk_deg_gonia_x.py
@@ -21,24 +21,44 @@ def main():
     df = get_plot_data(chroms)
     pvals = get_pvals(chroms)
 
+    # I am using two axes to simulate an axis break
     plt.style.use('scripts/figure_styles.mplstyle')
-    fig, ax = plt.subplots(figsize=(1, 2))
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(1, 2), gridspec_kw=dict(hspace=0.1), sharex=True)
 
-    df.plot(kind='bar', stacked=True, legend=False, width=.9, color=['C0', 'gray', 'r'], ax=ax)
-    ax.set_xticklabels([
-        l.get_text().replace('chr', '')
-        for l in ax.get_xticklabels()
-    ], rotation=0, fontsize=8)
+    # bar plots
+    df.plot(kind='bar', stacked=True, legend=False, width=.9, color=['C0', 'gray', 'r'], ax=ax1)
+    df.plot(kind='bar', stacked=True, legend=False, width=.9, color=['C0', 'gray', 'r'], ax=ax2)
 
+    ax1.set(ylim=(91, 100))
+    ax2.set(ylim=(0, 9))
+
+    # Add axis breaks
+    d = 0.015
+    kwargs = dict(transform=ax1.transAxes, clip_on=False, color='k', lw=1)
+    ax1.plot((d, -d), (d, - d), **kwargs)
+    kwargs.update(transform=ax2.transAxes)
+    ax2.plot((d, -d), (1 + d, 1 - d), **kwargs)
+
+    # Add p-values if needed
     if pvals.loc[('testis', 'flag_sig'), 'chrX'] == 1:
-        ax.text(0, df.loc['chrX', 'testis'] - 1, '*', color='w', ha='center', va='top', fontsize=12, fontweight='bold')
+        ax2.text(0, df.loc['chrX', 'testis'] - 1, '*', color='w', ha='center', va='top', fontsize=12, fontweight='bold')
 
     if pvals.loc[('testis', 'flag_sig'), 'chr4'] == 1:
-        ax.text(5, df.loc['chr4', 'testis'] - 1, '*', color='w', ha='center', va='top', fontsize=12, fontweight='bold')
+        ax2.text(5, df.loc['chr4', 'testis'] - 1, '*', color='w', ha='center', va='top', fontsize=12, fontweight='bold')
 
-    ax.margins(0)
-    sns.despine(ax=ax, left=True)
-    ax.set_ylabel('% Genes')
+    # Clean up axes
+    ax1.xaxis.set_visible(False)
+    ax1.margins(0)
+    sns.despine(ax=ax1, bottom=True)
+
+    ax2.set_xticklabels([
+        l.get_text().replace('chr', '')
+        for l in ax2.get_xticklabels()
+    ], rotation=0, fontsize=8)
+    ax2.margins(0)
+
+    # Add ylabel to span the two axis
+    fig.text(0, 0.5, '% Genes', rotation=90, va='center', ha='right', fontsize=plt.rcParams['axes.labelsize'])
 
     fig.savefig(oname, bbox_inches='tight')
 
@@ -47,7 +67,7 @@ def get_plot_data(chroms):
     gonia_bias = get_gonia_biased()
     bulk_sig = get_bulk_sig().reindex(gonia_bias)
 
-    df = bulk_sig.join(chroms).groupby('chrom').bias.value_counts().unstack()
+    df = bulk_sig.join(chroms, how='outer').fillna("None").groupby('chrom').bias.value_counts().unstack()
     df = df.div(df.sum(axis=1), axis='rows') * 100
     return df.loc[['chrX', 'chr2L', 'chr2R', 'chr3L', 'chr3R', 'chr4'], ['testis', 'None', 'ovary']]
 
@@ -57,7 +77,8 @@ def get_pvals(chroms):
     bulk_sig = get_bulk_sig().reindex(gonia_bias)
     df = (
         bulk_sig
-        .join(chroms)
+        .join(chroms, how='outer')
+        .fillna("None")
         .groupby('chrom').bias.value_counts()
         .unstack()
         .loc[['chrX', 'chr2L', 'chr2R', 'chr3L', 'chr3R', 'chr4']]
@@ -69,9 +90,9 @@ def get_pvals(chroms):
 def get_bulk_sig():
     bulk_sig = (
         pd.read_csv(bulk_fname, sep='\t', index_col=0)
-            .dropna()
-            .assign(testis_bias=lambda df: (df.log2FoldChange >= 1) & (df.padj <= 0.01))
-            .assign(ovary_bias=lambda df: (df.log2FoldChange <= -1) & (df.padj <= 0.01))
+        .dropna()
+        .assign(testis_bias=lambda df: (df.log2FoldChange >= 1) & (df.padj <= 0.01))
+        .assign(ovary_bias=lambda df: (df.log2FoldChange <= -1) & (df.padj <= 0.01))
     )
 
     bulk_sig.loc[bulk_sig.testis_bias, 'bias'] = 'testis'
