db229d73dd982603937ebbc9a18a29c37e008aa0
Justin M. Fear <justin.m.fear@gmail.com>
2019-04-19 08:43:17 -0400

    Changes plot to use black and white.
    I kept getting push back from people because we are already using blue
    and red for cysts cells and germline. I changed this to a black and white
    version to appease them.

--- science_submission/scripts/plot_barplot_bulk_demasculinization.py
+++ science_submission/scripts/plot_barplot_bulk_demasculinization.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from larval_gonad.stats import run_chisq
+from larval_gonad.plotting import  format_pval
 
 fname = snakemake.input.deg
 fbgn2chrom = snakemake.input.fbgn2chrom
@@ -18,27 +19,25 @@ oname = snakemake.output[0]
 def main():
     chroms = pd.read_parquet(fbgn2chrom)
     df = get_plot_data(chroms)
-    pvals = get_pvals(chroms)
 
     plt.style.use('scripts/figure_styles.mplstyle')
     fig, ax = plt.subplots(figsize=(1, 2))
 
-    df.plot(kind='bar', stacked=True, legend=False, width=.9, color=['C0', 'gray', 'r'], ax=ax)
-    ax.set_xticklabels([
-        l.get_text().replace('chr', '')
-        for l in ax.get_xticklabels()
-    ], rotation=0, fontsize=7)
+    # Plot proportion
+    df.plot(kind='bar', stacked=True, width=.9, color=['k', 'darkgray', 'w'], edgecolor='k', lw=.5, ax=ax)
+    ax.set_xticklabels([l.get_text().replace('chr', '') for l in ax.get_xticklabels()], rotation=0, fontsize=7)
+    plt.legend(['Testis-Biased', 'Not Biased', 'Ovary-Biased'], loc='upper left', bbox_to_anchor=[1, 1], frameon=False)
 
-    if pvals.loc[('testis', 'flag_sig'), 'chrX'] == 1:
-        ax.text(0, df.loc['chrX', 'testis'] - 1, '*', color='w', ha='center', va='top', fontsize=12, fontweight='bold')
+    # Add * for significance.
+    pvals = get_pvals(chroms)
+    kwargs = dict(va='top', fontsize=7, fontweight='bold', color='w')
+    format_pval(ax, 0, df.loc['chrX', 'testis'] - .01, pvals.loc[('testis', 'fdr q-value'), 'chrX'], **kwargs)
+    format_pval(ax, 5, df.loc['chr4', 'testis'] - .01, pvals.loc[('testis', 'fdr q-value'), 'chr4'], **kwargs)
 
-    if pvals.loc[('testis', 'flag_sig'), 'chr4'] == 1:
-        ax.text(5, df.loc['chr4', 'testis'] - 1, '*', color='w', ha='center', va='top', fontsize=12, fontweight='bold')
-
+    # Clean-up axes
+    sns.despine(ax=ax)
     ax.margins(0)
-    sns.despine(ax=ax, left=True)
-    ax.set_ylabel('% Genes')
-    ax.set_title("Bulk")
+    ax.set(ylabel='Proportion of Genes', title='Bulk')
 
     fig.savefig(oname, bbox_inches='tight')
 
@@ -47,7 +46,7 @@ def get_plot_data(chroms):
     bulk_sig = get_bulk_sig()
 
     df = bulk_sig.join(chroms, how='outer').fillna("None").groupby('chrom').bias.value_counts().unstack()
-    df = df.div(df.sum(axis=1), axis='rows') * 100
+    df = df.div(df.sum(axis=1), axis='rows')
     return df.loc[['chrX', 'chr2L', 'chr2R', 'chr3L', 'chr3R', 'chr4'], ['testis', 'None', 'ovary']]
 
 
@@ -67,9 +66,9 @@ def get_pvals(chroms):
 def get_bulk_sig():
     bulk_sig = (
         pd.read_csv(fname, sep='\t', index_col=0)
-            .dropna()
-            .assign(testis_bias=lambda df: (df.log2FoldChange >= 1) & (df.padj <= 0.01))
-            .assign(ovary_bias=lambda df: (df.log2FoldChange <= -1) & (df.padj <= 0.01))
+        .dropna()
+        .assign(testis_bias=lambda df: (df.log2FoldChange >= 1) & (df.padj <= 0.01))
+        .assign(ovary_bias=lambda df: (df.log2FoldChange <= -1) & (df.padj <= 0.01))
     )
 
     bulk_sig.loc[bulk_sig.testis_bias, 'bias'] = 'testis'
