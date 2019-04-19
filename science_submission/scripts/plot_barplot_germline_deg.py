182c2301fbb246add6d7549501a1df6c4b26dc27
Justin M. Fear <justin.m.fear@gmail.com>
2019-04-19 08:23:15 -0400

    Changes plot layout.
    * Aggregates individual chromosomes into a single 'A'
    * Removes the L1° panel
    * Changes back to horizontal layout
    * Changes color from red to darkgray.

--- science_submission/scripts/plot_barplot_germline_deg.py
+++ science_submission/scripts/plot_barplot_germline_deg.py
@@ -8,6 +8,8 @@ from scipy.stats import fisher_exact
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from larval_gonad.plotting import format_pval
+
 gonia = snakemake.input.gonia
 early = snakemake.input.early
 mid = snakemake.input.mid
@@ -17,34 +19,23 @@ background = snakemake.input.background
 
 oname = snakemake.output[0]
 
-AUTOSOMES = ['chr2L', 'chr2R', 'chr3L', 'chr3R']
-COLORS = ['#e50000', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#e50000']
-GERMLINE = ['SP', 'E1°', 'M1°', 'L1°']
+GERMLINE = ['SP', 'E1°', 'M1°']
 QUERY_MAPPER = {
     'SP': {
         'fname': gonia,
         'query': 'p_val_adj <= 0.01 & avg_logFC > 0',
-        'title': 'SP vs\nE1° M1° L1°',
-        'yaxis': 'Proportion\nGonia-Baised'
+        'title': 'SP-Biased',
     },
     'E1°': {
         'fname': early,
         'query': 'p_val_adj <= 0.01 & avg_logFC < 0',
-        'title': 'SP vs E1°',
-        'yaxis': 'Proportion\nE1°-Baised'
+        'title': 'E1°-Biased',
     },
     'M1°': {
         'fname': mid,
         'query': 'p_val_adj <= 0.01 & avg_logFC < 0',
-        'title': 'E1° vs M1°',
-        'yaxis': 'Proportion\nM1°-Baised'
+        'title': 'M1°-Biased',
     },
-    'L1°': {
-        'fname': late,
-        'query': 'p_val_adj <= 0.01 & avg_logFC < 0',
-        'title': 'M1° vs L1°',
-        'yaxis': 'Proportion\nL1°-Baised'
-    }
 }
 
 
@@ -52,36 +43,31 @@ def main():
     chroms = get_chroms()
 
     plt.style.use('scripts/figure_styles.mplstyle')
-    fig, axes = plt.subplots(4, 1, figsize=(1, 4), sharex=True, sharey=True, gridspec_kw=dict(hspace=.18))
+    fig, axes = plt.subplots(1, 3, figsize=(4, 1), sharex=True, sharey=True, gridspec_kw=dict(hspace=.18))
 
     for name, ax in zip(GERMLINE, axes):
+        # Get DEG
         significant_fbgns = get_significant_fbgns(name)
         ct = make_crosstab(significant_fbgns, chroms)
-        _, xpval = fisher_exact(ct[['chrX', 'autosome']])
-        _, fourpval = fisher_exact(ct[['chr4', 'autosome']])
-        proportion_genes_significant = ct.div(ct.sum()).loc[True, ['chrX', ] + AUTOSOMES + ['chr4', ]]
-        proportion_genes_significant.plot(kind='bar', width=.8, edgecolor='k', lw=.5, color=COLORS, ax=ax)
+
+        # Plot proportion of genes
+        proportion_genes_significant = ct.div(ct.sum()).loc[True, ['X', 'A', '4']]
+        proportion_genes_significant.plot(kind='bar', width=.8, edgecolor='k', lw=.5,
+                                          color=['darkgray', 'w', 'darkgray'], ax=ax)
 
         # Add * for significance
-        if xpval < 0.05:
-            ax.text(0, proportion_genes_significant['chrX'], '*', ha='center', va='center')
+        _, xpval = fisher_exact(ct[['X', 'A']])
+        _, fourpval = fisher_exact(ct[['4', 'A']])
+        format_pval(ax, 0, proportion_genes_significant['X'], xpval, fontsize=7)
+        format_pval(ax, 2, proportion_genes_significant['4'], fourpval, fontsize=7)
 
-        if fourpval < 0.05:
-            ax.text(5, proportion_genes_significant['chr4'], '*', ha='center', va='center')
-
-        # Add title
-        ax.text(0.5, .8, QUERY_MAPPER[name]['title'], transform=ax.transAxes, ha='center', va='top', fontsize=6)
         # Clean up axes
         sns.despine(ax=ax)
-        ax.set_ylim(0, .25)
+        ax.set(ylim=(0, .25), ylabel='Proportion of Genes', xlabel='')
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=0)
 
-        ax.set_xticklabels([
-            l.get_text().replace('chr', '')
-            for l in ax.get_xticklabels()
-        ], rotation=0)
-
-        ax.set_xlabel('')
-        ax.set_ylabel(QUERY_MAPPER[name]['yaxis'])
+        # Add title
+        ax.text(0.5, 1, QUERY_MAPPER[name]['title'], transform=ax.transAxes, ha='center', va='top', fontsize=6)
 
     fig.savefig(oname, bbox_inches='tight')
 
@@ -90,15 +76,27 @@ def get_chroms():
     with open(background, 'rb') as fh:
         bg = pickle.load(fh)
 
-    return pd.read_parquet(fbgn2chrom).reindex(bg).query('chrom != ["chrM", "chrY"]')
+    return (
+        pd.read_parquet(fbgn2chrom)
+        .replace({
+            'chrX': 'X',
+            'chr2L': 'A',
+            'chr2R': 'A',
+            'chr3L': 'A',
+            'chr3R': 'A',
+            'chr4': '4',
+        })
+        .reindex(bg)
+        .query('chrom != ["chrM", "chrY"]')
+    )
 
 
 def get_significant_fbgns(name):
     return (
         pd.read_csv(QUERY_MAPPER[name]['fname'], sep='\t', index_col=0)
-            .rename_axis('FBgn')
-            .query(QUERY_MAPPER[name]['query'])
-            .index.tolist()
+        .rename_axis('FBgn')
+        .query(QUERY_MAPPER[name]['query'])
+        .index.tolist()
     )
 
 
@@ -109,10 +107,9 @@ def make_crosstab(fbgns, chroms):
 
     return (
         _chroms
-            .groupby('biased').chrom.value_counts()
-            .unstack()
-            .assign(autosome=lambda df: df[AUTOSOMES].sum(axis=1))
-            .fillna(0)
+        .groupby('biased').chrom.value_counts()
+        .unstack()
+        .fillna(0)
     )
 
 
