4ea3228707770f036404e3fb7b2aab8fff8e2dda
Justin M. Fear <justin.m.fear@gmail.com>
2019-04-19 09:03:22 -0400

    Change colors and presentationt to match bulk demasculinization.

--- science_submission/scripts/plot_barplot_gonia_demasculinization.py
+++ science_submission/scripts/plot_barplot_gonia_demasculinization.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from larval_gonad.stats import run_chisq
+from larval_gonad.plotting import format_pval
 
 bulk_fname = snakemake.input.bulk_deg
 sc_fname = snakemake.input.sc_deg
@@ -19,18 +20,22 @@ oname = snakemake.output[0]
 def main():
     chroms = pd.read_parquet(fbgn2chrom)
     df = get_plot_data(chroms)
-    pvals = get_pvals(chroms)
 
     # I am using two axes to simulate an axis break
     plt.style.use('scripts/figure_styles.mplstyle')
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(1, 2), gridspec_kw=dict(hspace=0.1), sharex=True)
 
-    # bar plots
-    df.plot(kind='bar', stacked=True, legend=False, width=.9, color=['C0', 'gray', 'r'], ax=ax1)
-    df.plot(kind='bar', stacked=True, legend=False, width=.9, color=['C0', 'gray', 'r'], ax=ax2)
+    # Plot proportion
+    df.plot(kind='bar', stacked=True, legend=True, width=.9, color=['w', 'darkgray', 'k'], edgecolor='k', lw=.3, ax=ax1)
+    df.plot(kind='bar', stacked=True, legend=False, width=.9, color=['w', 'darkgray', 'k'], edgecolor='k', lw=.3, ax=ax2)
+    ax1.legend(['Testis-Biased', 'Not Biased', 'Ovary-Biased'], loc='upper left', bbox_to_anchor=[1, 1], frameon=False)
 
-    ax1.set(ylim=(91, 100))
-    ax2.set(ylim=(0, 9))
+    # Add * for significance
+    pvals = get_pvals(chroms)
+    kwargs = dict(va='top', fontsize=7, color='k')
+    format_pval(ax2, 0, df.loc['chrX', 'testis'] - .01, pvals.loc[('testis', 'fdr q-value'), 'chrX'], **kwargs)
+    format_pval(ax2, 5, df.loc['chr4', 'testis'] - .01, pvals.loc[('testis', 'fdr q-value'), 'chr4'], **kwargs)
+
 
     # Add axis breaks
     d = 0.015
@@ -39,26 +44,20 @@ def main():
     kwargs.update(transform=ax2.transAxes)
     ax2.plot((d, -d), (1 + d, 1 - d), **kwargs)
 
-    # Add p-values if needed
-    if pvals.loc[('testis', 'flag_sig'), 'chrX'] == 1:
-        ax2.text(0, df.loc['chrX', 'testis'] - 1, '*', color='w', ha='center', va='top', fontsize=12, fontweight='bold')
-
-    if pvals.loc[('testis', 'flag_sig'), 'chr4'] == 1:
-        ax2.text(5, df.loc['chr4', 'testis'] - 1, '*', color='w', ha='center', va='top', fontsize=12, fontweight='bold')
 
     # Clean up axes
+    ax1.set(ylim=(.91, 1))
     ax1.xaxis.set_visible(False)
     ax1.margins(0)
     sns.despine(ax=ax1, bottom=True)
 
-    ax2.set_xticklabels([
-        l.get_text().replace('chr', '')
-        for l in ax2.get_xticklabels()
-    ], rotation=0, fontsize=7)
+    ax2.set(ylim=(0, .09))
+    ax2.set_xticklabels([l.get_text().replace('chr', '') for l in ax2.get_xticklabels()], rotation=0, fontsize=7)
     ax2.margins(0)
+    sns.despine(ax=ax2)
 
-    # Add ylabel to span the two axis
-    fig.text(0, 0.5, '% Genes', rotation=90, va='center', ha='right', fontsize=plt.rcParams['axes.labelsize'])
+    # Add y-label to span the two axis
+    fig.text(-0.05, 0.5, 'Proportion of Genes', rotation=90, va='center', ha='right', fontsize=plt.rcParams['axes.labelsize'])
     ax1.set_title("Gonia-bias")
 
     fig.savefig(oname, bbox_inches='tight')
@@ -69,7 +68,7 @@ def get_plot_data(chroms):
     bulk_sig = get_bulk_sig().reindex(gonia_bias)
 
     df = bulk_sig.join(chroms, how='outer').fillna("None").groupby('chrom').bias.value_counts().unstack()
-    df = df.div(df.sum(axis=1), axis='rows') * 100
+    df = df.div(df.sum(axis=1), axis='rows')
     return df.loc[['chrX', 'chr2L', 'chr2R', 'chr3L', 'chr3R', 'chr4'], ['testis', 'None', 'ovary']]
 
 
