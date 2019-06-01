65073843f353a6558f9beead3c7e170f386d1d11
Justin M. Fear <justin.m.fear@gmail.com>
2019-06-01 16:10:11 -0400

    Fixes 'chr' prefix.

--- cellranger3-wf/scripts/parse_bam.py
+++ cellranger3-wf/scripts/parse_bam.py
@@ -16,13 +16,13 @@ def main():
     sam_file_handler = pysam.AlignmentFile(BAM_FILE, 'rb')
 
     chrom_counts_by_cell = []
-    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, '2L'))
-    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, '2R'))
-    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, '3L'))
-    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, '3R'))
-    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, '4'))
-    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, 'X'))
-    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, 'Y'))
+    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, 'chr2L'))
+    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, 'chr2R'))
+    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, 'chr3L'))
+    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, 'chr3R'))
+    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, 'chr4'))
+    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, 'chrX'))
+    chrom_counts_by_cell.extend(fetch_reads_from_chromosome(sam_file_handler, 'chrY'))
 
     all_counts_sorted = sorted(chrom_counts_by_cell, key=lambda x: (x[0], x[1]))
     del chrom_counts_by_cell
