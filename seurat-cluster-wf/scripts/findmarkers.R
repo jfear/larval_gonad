3fde75c50ee2e92f67caf8bd4f35f484ae7f5631
Justin M. Fear <justin.m.fear@gmail.com>
2019-06-05 08:12:29 -0400

    Find markers for each res separately.
    I changed it so each resolution is run as a different job. This
    will speed things up a bit.

--- seurat-cluster-wf/scripts/findmarkers.R
+++ seurat-cluster-wf/scripts/findmarkers.R
@@ -4,30 +4,32 @@ source('../lib/seurat.R')
 
 # Get snakemake resources
 WORKDIR <- dirname(snakemake@input[[1]])
-RESOLUTIONS <- snakemake@params[['resolutions']]
+RESOLUTION <- snakemake@wildcards[['resolution']]
 
 REFERENCES_DIR <- Sys.getenv('REFERENCES_DIR', '/data/LCDB/lcdb-references')
-fbgn2gene <- read.table(
+fbgn2symbol <- read.table(
     file.path(REFERENCES_DIR, 'dmel/r6-24/fb_annotation/dmel_r6-24.fb_annotation'),
-    sep='\t',
-    stringsAsFactors=F,
-    header=T,
-    quote=NULL,
-    fill=TRUE,
-    colClasses=c("character", "NULL", "character", rep("NULL", 6))
+    sep = '\t',
+    stringsAsFactors = FALSE,
+    header = TRUE,
+    quote = NULL,
+    fill = TRUE,
+    colClasses = c("character", "NULL", "character", rep("NULL", 6))
 ) %>% distinct()
 
 # Load the Seurat Object
 load(file.path(WORKDIR, 'seurat.Robj'))
 
-# Run differential expression
-for (i in RESOLUTIONS){
-  name <- paste0('res.', i)
-  sobj <- SetAllIdent(sobj, id = name)
-
-  # Get markers ignoring replicate.
-  fname <- paste0('biomarkers_', name, '.tsv')
-  markers <- FindAllMarkers(object = sobj, only.pos = TRUE, print.bar = FALSE)
-  markers = merge(fbgn2symbol, markers, by.x='primary_FBgn', by.y='gene', all.y=T)
-  save_biomarkers(markers = markers, dir = OUTDIR, fname = fname)
+if (snakemake@wildcards[['sample']] == 'combined'){
+  sobj <- combined
 }
+
+# Run differential expression
+name <- paste0('res.', RESOLUTION)
+sobj <- SetAllIdent(sobj, id = name)
+
+# Get markers 
+fname <- paste0('biomarkers_', name, '.tsv')
+markers <- FindAllMarkers(object = sobj, only.pos = TRUE, print.bar = FALSE)
+markers <- merge(fbgn2symbol, markers, by.x = 'primary_FBgn', by.y = 'gene', all.y = TRUE)
+save_biomarkers(markers = markers, dir = WORKDIR, fname = fname)
