65113f428f36dcde8aa5e4ed681ad5464cd81391
Justin M. Fear <justin.m.fear@gmail.com>
2019-06-13 14:27:34 -0400

    Updates filter cells to use new gene annotations.

--- cellselection-wf/scripts/filter_cells.py
+++ cellselection-wf/scripts/filter_cells.py
@@ -7,7 +7,7 @@ cell_calls = snakemake.input.cell_calls
 cell_ids = snakemake.input.cell_ids
 genes = snakemake.input.genes
 mtx = snakemake.input.mtx
-fbgn2symbol_ = snakemake.input.fbgn2symbol
+gene_annotation = snakemake.input.gene_annotation
 
 barcodes = snakemake.output.barcodes
 genes_out = snakemake.output.genes
@@ -18,39 +18,36 @@ def main():
     # Remove bad cells from cell ids.
     idx = get_cell_id_index()
     ids = _read_text(cell_ids)[idx]
-    with open(barcodes, 'w') as fout:
-        fout.write('\n'.join(ids))
+    with open(barcodes, "w") as fout:
+        fout.write("\n".join(ids))
 
     # Remove bad cells from matrix
     mm = scipy.io.mmread(mtx).tocsc()[:, idx].tocoo()
     scipy.io.mmwrite(mtx_out, mm)
 
     # Add gene symbol
-    fbgn2symbol = (pd.read_feather(fbgn2symbol_)
-        .set_index('FBgn')
-    )
+    fbgn2symbol = pd.read_feather(gene_annotation, columns=['FBgn', 'gene_symbol']).set_index("FBgn")
 
-    (pd.read_csv(genes, sep='\t', header=None, index_col=0)
-        .rename_axis('FBgn')
-        .join(fbgn2symbol, how='left')
-        .gene_symbol
-        .reset_index()
-        .to_csv(genes_out, sep='\t', index=None, header=None)
+    (
+        pd.read_csv(genes, sep="\t", header=None, index_col=0)
+        .rename_axis("FBgn")
+        .join(fbgn2symbol, how="left")
+        .gene_symbol.reset_index()
+        .to_csv(genes_out, sep="\t", index=None, header=None)
     )
 
 
 def _read_text(fname):
     with open(fname) as fh:
-        return np.array(fh.read().strip().split('\n'))
+        return np.array(fh.read().strip().split("\n"))
 
 
 def get_intersection_and_dedub():
     df_cell_calls = (
-        pd.read_feather(cell_calls)
-        .set_index('cell_id')
+        pd.read_feather(cell_calls).set_index("cell_id")
         # calculate the intersection of cellranger3-wf and droputils
-        .loc[:, ['cellranger3-wf', 'droputils']]
-        .sum(axis=1) == 2
+        .loc[:, ["cellranger3-wf", "droputils"]].sum(axis=1)
+        == 2
     )
 
     dublet_ids = _read_text(scrublet)
@@ -68,4 +65,4 @@ def get_cell_id_index():
 
 if __name__ == "__main__":
     main()
-    
+
