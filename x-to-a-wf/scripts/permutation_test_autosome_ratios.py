db0346bbabe9d104ea9e240aa0fbace590230289
Justin M. Fear <justin.m.fear@gmail.com>
2019-06-25 12:38:12 -0400

    Updated X-to-A workflow.

--- x-to-a-wf/scripts/permutation_test_autosome_ratios.py
+++ x-to-a-wf/scripts/permutation_test_autosome_ratios.py
@@ -1,28 +1,39 @@
 import pandas as pd
 from scipy.stats import mannwhitneyu
 
-fname = snakemake.input[0]
-oname = snakemake.output[0]
+FNAME = snakemake.input[0]
+ONAME = snakemake.output[0]
+
+# Debug Settings
+# FNAME = "output/x-to-a-wf/autosome_ratios_by_cell.feather"
 
 
 def main():
-    ratios = pd.read_parquet(fname)
+    ratios = pd.read_feather(FNAME).set_index("cell_id")
     _ratios = ratios.copy()
 
     results = []
     for i in range(10_000):
-        _ratios['cluster'] = _ratios.cluster.sample(frac=1).values
-        for cluster, dd in _ratios.groupby('cluster'):
+        _ratios["cluster"] = _ratios.cluster.sample(frac=1).values
+        for cluster, dd in _ratios.groupby("cluster"):
             obs = ratios.query(f'cluster == "{cluster}"')
-            _, pval_x = mannwhitneyu(obs.x_to_a_ratio, dd.x_to_a_ratio, alternative='less')
-            _, pval_4 = mannwhitneyu(obs.fourth_to_a_ratio, dd.fourth_to_a_ratio, alternative='less')
-            _, pval_y = mannwhitneyu(obs.y_to_a_ratio, dd.y_to_a_ratio, alternative='greater')
+            _, pval_x = mannwhitneyu(
+                obs.x_to_a_ratio, dd.x_to_a_ratio, alternative="less"
+            )
+            _, pval_4 = mannwhitneyu(
+                obs.fourth_to_a_ratio, dd.fourth_to_a_ratio, alternative="less"
+            )
+            _, pval_y = mannwhitneyu(
+                obs.y_to_a_ratio, dd.y_to_a_ratio, alternative="greater"
+            )
             results.append((cluster, pval_x <= 0.05, pval_4 <= 0.05, pval_y <= 0.05))
 
-    df = pd.DataFrame(results, columns=['cluster', 'pval_x', 'pval_4', 'pval_y'])
-    pvals = 1 - df.groupby('cluster').mean()
-    pvals.to_parquet(oname)
+    df = pd.DataFrame(results, columns=["cluster", "pval_x", "pval_4", "pval_y"])
+    pvals = 1 - df.groupby("cluster").mean().reindex(ratios.cluster.cat.categories)
+    pvals.reset_index().to_feather(ONAME)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
+pd.Series.cat
