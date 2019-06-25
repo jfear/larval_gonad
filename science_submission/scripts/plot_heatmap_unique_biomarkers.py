5c7ac03938824758aab0156c9c17c56d62c1eb73
Justin M. Fear <justin.m.fear@gmail.com>
2019-06-25 18:41:59 -0400

    Starts the unique heatmap.

--- science_submission/scripts/plot_heatmap_unique_biomarkers.py
+++ science_submission/scripts/plot_heatmap_unique_biomarkers.py
@@ -1,152 +1,128 @@
-"""Plot heatmap of all genes.
-
-Plots the tpm normalized zscores of all genes as a heatmap.
-"""
+"""Plot heatmap of unique biomarkers"""
 import matplotlib
 
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 
 from itertools import chain
-from pickle import loads
-import numpy as np
+from more_itertools import flatten
 import pandas as pd
 from matplotlib.gridspec import GridSpec
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-fname = snakemake.input.zscores
-fbgn2symbol = snakemake.input.fbgn2symbol
-biomarkers = snakemake.input.biomarkers
+from scipy.cluster.hierarchy import linkage, dendrogram
 
-oname = snakemake.output[0]
+from larval_gonad.io import feather_to_cluster_rep_matrix
 
-annotation = snakemake.params.annotation
-cluster_order = snakemake.params.cluster_order
-cluster_colors = snakemake.params.cluster_colors
-cmap = snakemake.params.cmap
+FNAME = snakemake.input.zscores
+GENE_METADATA = snakemake.input.gene_metadata
+BIOMARKERS = snakemake.input.biomarkers
+
+CLUSTER_ANNOT = snakemake.params.cluster_annot
+CLUSTER_ORDER = snakemake.params.cluster_order
+CMAP = snakemake.params.cmap
+
+ONAME = snakemake.output[0]
+
+# Debug settings
+FNAME = "output/science_submission/zscore_by_cluster_rep.feather"
+GENE_METADATA = "references/gene_annotation_dmel_r6-24.feather"
+BIOMARKERS = "output/seurat3-cluster-wf/combined_n3_biomarkers.feather"
+import yaml
+config = yaml.safe_load(open('config/common.yaml'))
+CLUSTER_ANNOT = config['cluster_annot']
+CLUSTER_ORDER = config['cluster_order']
+CMAP = "viridis"
 
 
 def main():
-    df, unique_genes_per_cluster = get_data()
 
-    plt.style.use('scripts/figure_styles.mplstyle')
-    fig = plt.figure(figsize=(3, 4))
-    gs = GridSpec(2, 1, height_ratios=[1, .02])
-    ax = fig.add_subplot(gs[0, 0])
-    cax = fig.add_subplot(gs[1, 0])
-    sns.heatmap(
-        df,
-        xticklabels=True,
-        yticklabels=False,
-        vmin=-3,
-        vmax=3,
-        rasterized=False,
-        cmap=cmap,
-        ax=ax,
-        cbar_ax=cax,
-        cbar_kws=dict(label='Z-Score (TPM)', ticks=[-3, 0, 3], orientation='horizontal')
-    )
+fbgn2symbol = (
+    pd.read_feather(GENE_METADATA, columns=["FBgn", "gene_symbol"])
+    .set_index("FBgn")
+    .to_dict()["gene_symbol"]
+)
 
-    # Clean up X axis
-    ax.set_xlabel('')
-    ax.xaxis.set_ticks_position('top')
-    ax.set_xticklabels(list(chain.from_iterable([('', x, '') for x in cluster_order])), ha='center', va='bottom')
+biomarkers = (
+    pd.read_feather(BIOMARKERS, columns=['FBgn', 'cluster'])
+    .assign(cluster=lambda df: df.cluster.cat.rename_categories(CLUSTER_ANNOT))
+    .assign(cluster=lambda df: df.cluster.cat.reorder_categories(CLUSTER_ORDER))
+    .drop_duplicates(subset="FBgn", keep=False)
+)
 
-    # Add additional x annotations
-    yloc = 0 - (df.shape[0] * .05)
-    pad = yloc * .1
-    ax.text(6, yloc + pad, 'Germline', ha='center', fontsize=6, color=cluster_colors[0], va='bottom')
-    ax.text(17, yloc + pad, 'Somatic\nCyst', ha='center', fontsize=6, color=cluster_colors[4], va='bottom')
-    ax.text(24, yloc + pad, 'Somatic\nOther', ha='center', fontsize=6, color=cluster_colors[8], va='bottom')
-    ax.text(31, yloc + pad, 'Unknown', ha='center', fontsize=6, color=cluster_colors[-1], va='bottom')
-    lines = [
-        plt.Line2D([0, 12], [yloc, yloc], color=cluster_colors[0], lw=1.5, clip_on=False),
-        plt.Line2D([12, 21], [yloc, yloc], color=cluster_colors[4], lw=1.5, clip_on=False),
-        plt.Line2D([21, 24], [yloc, yloc], color=cluster_colors[7], lw=1.5, clip_on=False),
-        plt.Line2D([24, 27], [yloc, yloc], color=cluster_colors[8], lw=1.5, clip_on=False),
-        plt.Line2D([27, 35], [yloc, yloc], color=cluster_colors[-1], lw=1.5, clip_on=False),
-    ]
+zscores = feather_to_cluster_rep_matrix(FNAME).reindex(biomarkers.FBgn)
 
-    for l in lines:
-        ax.add_line(l)
+# cluster genes bases on expression
+link = linkage(zscores.values, 'average')
+tree = dendrogram(link, no_plot=True)
+leaves = tree['leaves']
+zscores = zscores.iloc[leaves, :]
 
-    # Add lines separating cell types
-    for i in range(1, 12):
-        ax.axvline(i * 3, color='w', ls='--', lw=.5)
+plt.style.use('scripts/figure_styles.mplstyle')
+fig = plt.figure(figsize=(4, 8))
+gs = GridSpec(2, 1, height_ratios=[1, .02])
+ax = fig.add_subplot(gs[0, 0])
+cax = fig.add_subplot(gs[1, 0])
+sns.heatmap(
+    zscores,
+    xticklabels=True,
+    yticklabels=False,
+    vmin=-3,
+    vmax=3,
+    rasterized=False,
+    cmap=CMAP,
+    ax=ax,
+    cbar_ax=cax,
+    cbar_kws=dict(label='Z-Score (TPM)', ticks=[-3, 0, 3], orientation='horizontal')
+)
 
-    # Clean up Y axis
-    ax.set_ylabel('')
+# Clean up X axis
+ax.set_xlabel('')
+ax.xaxis.set_ticks_position('top')
+ax.set_xticklabels(list(chain.from_iterable([('', x, '') for x in cluster_order])), ha='center', va='bottom')
 
-    # Add lines separating biomarker groups
-    for loc in np.cumsum(unique_genes_per_cluster)[:-1]:
-        ax.axhline(loc, color='w', ls='--', lw=.5)
+# Add additional x annotations
+yloc = 0 - (df.shape[0] * .05)
+pad = yloc * .1
+ax.text(6, yloc + pad, 'Germline', ha='center', fontsize=6, color=cluster_colors[0], va='bottom')
+ax.text(17, yloc + pad, 'Somatic\nCyst', ha='center', fontsize=6, color=cluster_colors[4], va='bottom')
+ax.text(24, yloc + pad, 'Somatic\nOther', ha='center', fontsize=6, color=cluster_colors[8], va='bottom')
+ax.text(31, yloc + pad, 'Unknown', ha='center', fontsize=6, color=cluster_colors[-1], va='bottom')
+lines = [
+    plt.Line2D([0, 12], [yloc, yloc], color=cluster_colors[0], lw=1.5, clip_on=False),
+    plt.Line2D([12, 21], [yloc, yloc], color=cluster_colors[4], lw=1.5, clip_on=False),
+    plt.Line2D([21, 24], [yloc, yloc], color=cluster_colors[7], lw=1.5, clip_on=False),
+    plt.Line2D([24, 27], [yloc, yloc], color=cluster_colors[8], lw=1.5, clip_on=False),
+    plt.Line2D([27, 35], [yloc, yloc], color=cluster_colors[-1], lw=1.5, clip_on=False),
+]
 
-    # Add additional y annotations
-    loc = 0
-    for i, cnt in enumerate(unique_genes_per_cluster):
-        if i == 10:
-            x = 38
-        else:
-            x = 35
-        ax.text(-.5, loc + (cnt / 2), cluster_order[i], ha='right', va='center', fontweight='bold', fontsize=5.5)
-        ax.text(x, loc + (cnt / 2), f'n={cnt}', ha='left', va='center', fontweight='bold', fontsize=5.5)
-        loc += cnt
+for l in lines:
+    ax.add_line(l)
 
-    fig.savefig(oname, bbox_inches='tight')
+# Add lines separating cell types
+for i in range(1, 12):
+    ax.axvline(i * 3, color='w', ls='--', lw=.5)
 
+# Clean up Y axis
+ax.set_ylabel('')
 
-def get_data():
-    zscores = (
-        pd.read_parquet(fname)
-        .reset_index()
-        .assign(
-            cluster=lambda df: (
-                df.cluster.map(annotation)
-                # .pipe(lambda x: x[x != 'UNK'])
-                .astype('category')
-                .cat.as_ordered()
-                .cat.reorder_categories(cluster_order)
-            )
-        )
-        .assign(
-            rep=lambda df: (
-                df.rep.astype('category')
-                .cat.as_ordered()
-                .cat.reorder_categories(['rep1', 'rep2', 'rep3'])
-            )
-        )
-        .sort_values(by=['cluster', 'rep'])
-        .pivot_table(index='FBgn', columns=['cluster', 'rep'], values='tpm_zscore')
-    )
+# Add lines separating biomarker groups
+for loc in np.cumsum(unique_genes_per_cluster)[:-1]:
+    ax.axhline(loc, color='w', ls='--', lw=.5)
 
-    # Get biomarkers by cluster
-    _biomarkers = (
-        pd.read_csv(biomarkers, sep='\t', usecols=['primary_FBgn', 'cluster'], index_col=0)
-        .cluster
-        .map(annotation)
-        # .pipe(lambda x: x[x != "UNK"])
-        .astype('category')
-        .cat.as_ordered()
-        .cat.reorder_categories(cluster_order)
-        .rename_axis('FBgn')
-        .to_frame()
-    )
+# Add additional y annotations
+loc = 0
+for i, cnt in enumerate(unique_genes_per_cluster):
+    if i == 10:
+        x = 38
+    else:
+        x = 35
+    ax.text(-.5, loc + (cnt / 2), cluster_order[i], ha='right', va='center', fontweight='bold', fontsize=5.5)
+    ax.text(x, loc + (cnt / 2), f'n={cnt}', ha='left', va='center', fontweight='bold', fontsize=5.5)
+    loc += cnt
 
-    # pull out genes that were unique to a single cluster
-    unique_fbgns = _biomarkers.groupby('FBgn').size().pipe(lambda x: x[x == 1]).index
-    df = _biomarkers.query(f'FBgn == {unique_fbgns.tolist()}').sort_values(by='cluster')
-    fbgns = df.index
-    zscores = zscores.reindex(fbgns)
+fig.savefig(oname, bbox_inches='tight')
 
-    # Count the number of genes in each group
-    unique_genes_per_cluster = df.cluster.value_counts().sort_index().tolist()
-
-    # Convert to gene names
-    with open(fbgn2symbol, 'rb') as fh:
-        mapper = loads(fh.read())
-    zscores.index = zscores.index.map(mapper)
-
-    return zscores, unique_genes_per_cluster
 
 
 if __name__ == '__main__':
