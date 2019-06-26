c2c7a4c39e1644f7aa84316790d0cecf34ac7886
Justin Fear <justin.m.fear@gmail.com>
2019-06-26 15:07:10 -0400

    Updates biomarkers multi heatmap.

--- science_submission/scripts/plot_heatmap_multi_biomarkers.py
+++ science_submission/scripts/plot_heatmap_multi_biomarkers.py
@@ -1,46 +1,89 @@
-"""Plot heatmap of all genes.
-
-Plots the tpm normalized zscores of all genes as a heatmap.
-"""
+"""Plot heatmap of unique biomarkers"""
 import matplotlib
 
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 
-from itertools import chain, combinations
-from pickle import loads
-import numpy as np
+from itertools import chain
+from more_itertools import flatten
 import pandas as pd
 from matplotlib.gridspec import GridSpec
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-fname = snakemake.input.zscores
-biomarkers = snakemake.input.biomarkers
+from scipy.cluster.hierarchy import linkage, dendrogram
 
-oname = snakemake.output[0]
+from larval_gonad.io import feather_to_cluster_rep_matrix
 
-annotation = snakemake.params.annotation
-cluster_order = snakemake.params.cluster_order
-cluster_colors = snakemake.params.cluster_colors
-cmap = snakemake.params.cmap
+FNAME = snakemake.input.zscores
+GENE_METADATA = snakemake.input.gene_metadata
+BIOMARKERS = snakemake.input.biomarkers
+
+CLUSTER_ANNOT = snakemake.params.cluster_annot
+CLUSTER_ORDER = snakemake.params.cluster_order
+CLUSTER_TYPES = snakemake.params.cluster_types
+CMAP = snakemake.params.cmap
+
+ONAME = snakemake.output[0]
+
+# Debug settings
+# FNAME = "output/science_submission/zscore_by_cluster_rep.feather"
+# GENE_METADATA = "references/gene_annotation_dmel_r6-24.feather"
+# BIOMARKERS = "output/seurat3-cluster-wf/combined_n3_biomarkers.feather"
+# import yaml
+# config = yaml.safe_load(open('config/common.yaml'))
+# CLUSTER_ANNOT = config['cluster_annot']
+# CLUSTER_ORDER = config['cluster_order']
+# CLUSTER_TYPES = config['cluster_types']
+ 
+# CMAP = "viridis"
 
 
 def main():
-    df, multi_genes_per_cluster = get_data()
+    fbgn2symbol = (
+        pd.read_feather(GENE_METADATA, columns=["FBgn", "gene_symbol"])
+        .set_index("FBgn")
+        .to_dict()["gene_symbol"]
+    )
 
-    plt.style.use('scripts/paper_1c.mplstyle')
-    fig = plt.figure(figsize=(3, 4))
-    gs = GridSpec(2, 1, height_ratios=[1, .02], hspace=0.05)
+    biomarkers = (
+        pd.read_feather(BIOMARKERS, columns=['FBgn', 'cluster'])
+        .assign(cluster=lambda df: df.cluster.cat.rename_categories(CLUSTER_ANNOT))
+        .assign(cluster=lambda df: df.cluster.cat.reorder_categories(CLUSTER_ORDER))
+        .pipe(lambda df: df[df.FBgn.duplicated(keep=False)])
+        .assign(cell_type=lambda df: df.cluster.map(dict(zip(CLUSTER_ORDER, CLUSTER_TYPES))))
+    )
+
+    # Re-label based on cell type (Germ Only, Soma Only, Germ and Soma)
+    biomarkers_by_celltype = (
+        biomarkers.groupby('FBgn')
+        .apply(lambda df: df.cell_type.sort_values().str.cat(sep='|'))
+        .apply(cell_type_mapper)
+        .rename('cell_type')
+        .to_frame()
+        .reset_index()
+    )
+
+    zscores = feather_to_cluster_rep_matrix(FNAME).reindex(biomarkers.FBgn.unique())
+
+    # order zscores by doing a hierarchical cluster for each cell type group.
+    zscores_ordered = pd.concat((
+        hierarchal_cluster(zscores.reindex(v.FBgn.unique()))
+        for k, v in biomarkers_by_celltype.groupby('cell_type')
+    ))
+
+    plt.style.use('scripts/figure_styles.mplstyle')
+    fig = plt.figure(figsize=(4, 8))
+    gs = GridSpec(2, 1, height_ratios=[1, 0.01], hspace=0.01)
     ax = fig.add_subplot(gs[0, 0])
     cax = fig.add_subplot(gs[1, 0])
     sns.heatmap(
-        df,
+        zscores_ordered,
         xticklabels=True,
         yticklabels=False,
         vmin=-3,
         vmax=3,
-        rasterized=False,
-        cmap=cmap,
+        rasterized=True,
+        cmap=CMAP,
         ax=ax,
         cbar_ax=cax,
         cbar_kws=dict(label='Z-Score (TPM)', ticks=[-3, 0, 3], orientation='horizontal')
@@ -49,129 +92,44 @@ def main():
     # Clean up X axis
     ax.set_xlabel('')
     ax.xaxis.set_ticks_position('top')
-    ax.xaxis.set_tick_params(pad=0, length=2)
-    ax.set_xticklabels(list(chain.from_iterable([('', x, '') for x in cluster_order])), ha='center', va='bottom',
-                       fontsize=5.5)
-
-    # Add additional x annotations
-    yloc = 0 - (df.shape[0] * .05)
-    pad = yloc * .1
-    ax.text(6, yloc + pad, 'Germline', ha='center', fontsize=6, color=cluster_colors[0], va='bottom')
-    ax.text(17, yloc + pad, 'Somatic\nCyst', ha='center', fontsize=6, color=cluster_colors[4], va='bottom')
-    ax.text(24, yloc + pad, 'Somatic\nOther', ha='center', fontsize=6, color=cluster_colors[8], va='bottom')
-    ax.text(31, yloc + pad, 'Unknown', ha='center', fontsize=6, color=cluster_colors[-1], va='bottom')
-    lines = [
-        plt.Line2D([0, 12], [yloc, yloc], color=cluster_colors[0], lw=1.5, clip_on=False),
-        plt.Line2D([12, 21], [yloc, yloc], color=cluster_colors[4], lw=1.5, clip_on=False),
-        plt.Line2D([21, 24], [yloc, yloc], color=cluster_colors[7], lw=1.5, clip_on=False),
-        plt.Line2D([24, 27], [yloc, yloc], color=cluster_colors[8], lw=1.5, clip_on=False),
-        plt.Line2D([27, 35], [yloc, yloc], color=cluster_colors[-1], lw=1.5, clip_on=False),
-    ]
-
-    for l in lines:
-        ax.add_line(l)
+    ax.set_xticklabels(list(chain.from_iterable([('', x, '') for x in CLUSTER_ORDER])), ha='center', va='bottom')
 
     # Add lines separating cell types
-    for i in range(1, 12):
+    for i in range(1, len(CLUSTER_ORDER)):
         ax.axvline(i * 3, color='w', ls='--', lw=.5)
 
     # Clean up Y axis
     ax.set_ylabel('')
 
     # Add lines separating biomarker groups
-    for loc in np.cumsum(multi_genes_per_cluster.values)[:-1]:
-        ax.axhline(loc, color='w', ls='--', lw=.5)
-
-    # Add additional y annotations
     loc = 0
-    for label, cnt in multi_genes_per_cluster.iteritems():
-        if label == 'G':
-            _label = f'Germ Only\n(n={cnt:,})'
-        elif label == 'S':
-            _label = f'Soma Only\n(n={cnt:,})'
-        else:
-            _label = f'Germ and\nSoma\n(n={cnt:,})'
-        ax.text(-.5, loc + (cnt / 2), _label, ha='right', va='center', fontsize=5.5, fontweight='bold')
-        loc += cnt
+    xloc = zscores_ordered.shape[1] + 1
+    for clus, dd in biomarkers_by_celltype.groupby('cell_type'):
+        prev = loc
+        loc += dd.shape[0]
+        mid = loc - ((loc - prev) / 2)
+        ax.axhline(loc, color='w', ls='--', lw=.5)
+        txt = f'{clus} ({dd.shape[0]:,})'
+        ax.text(xloc, mid, txt, ha='left', va='center', fontweight='bold', fontsize=8)
 
-    # Clean up color bar
-    plt.setp(cax.xaxis.label, fontsize=6)
-    plt.setp(cax.get_xticklabels(), fontsize=5)
-    cax.xaxis.set_tick_params(pad=0, length=2)
-
-    fig.savefig(oname, bbox_inches='tight')
+    fig.savefig(ONAME, bbox_inches='tight')
 
 
-def get_data():
-    zscores = (
-        pd.read_parquet(fname)
-        .reset_index()
-        .assign(
-            cluster=lambda df: (
-                df.cluster.map(annotation)
-                # .pipe(lambda x: x[x != 'UNK'])
-                .astype('category')
-                .cat.as_ordered()
-                .cat.reorder_categories(cluster_order)
-            )
-        )
-        .assign(
-            rep=lambda df: (
-                df.rep.astype('category')
-                    .cat.as_ordered()
-                    .cat.reorder_categories(['rep1', 'rep2', 'rep3'])
-            )
-        )
-        .sort_values(by=['cluster', 'rep'])
-        .pivot_table(index='FBgn', columns=['cluster', 'rep'], values='tpm_zscore')
-    )
+def hierarchal_cluster(df):
+    # cluster genes bases on expression
+    link = linkage(df.values, 'average')
+    tree = dendrogram(link, no_plot=True)
+    leaves = tree['leaves']
+    return df.iloc[leaves, :]
 
-    # Get biomarkers by cluster
-    _biomarkers = (
-        pd.read_csv(biomarkers, sep='\t', usecols=['primary_FBgn', 'cluster'], index_col=0)
-        .cluster
-        .map(annotation)
-        # .pipe(lambda x: x[x != "UNK"])
-        .astype('category')
-        .cat.as_ordered()
-        .cat.reorder_categories(cluster_order)
-        .rename_axis('FBgn')
-        .to_frame()
-    )
 
-    # pull out genes that are in more than one cluster
-    multi_fbgns = _biomarkers.groupby('FBgn').size().pipe(lambda x: x[x > 1]).index
-    df = (
-        _biomarkers.query(f'FBgn == {multi_fbgns.tolist()}')
-        .groupby('FBgn')
-        .apply(lambda df: '|'.join(df.cluster.sort_values().values))
-    )
-
-    germ_combos = ['|'.join(x) for x in chain(
-        combinations(cluster_order[:4], 2),
-        combinations(cluster_order[:4], 3),
-        combinations(cluster_order[:4], 4),
-    )]
-
-    soma_combos = ['|'.join(x) for x in chain(
-        combinations(cluster_order[4:], 2),
-        combinations(cluster_order[4:], 3),
-        combinations(cluster_order[4:], 4),
-        combinations(cluster_order[4:], 5),
-    )]
-
-    flags = pd.Series(index=df.index).fillna('GS')
-    flags[df.isin(germ_combos)] = 'G'
-    flags[df.isin(soma_combos)] = 'S'
-    flags = flags.astype('category').cat.as_ordered().cat.reorder_categories(['G', 'GS', 'S'])
-
-    fbgns = flags.sort_values().index
-    zscores = zscores.reindex(fbgns).dropna()
-
-    # Count the number of genes in each group
-    multi_genes_per_cluster = flags.value_counts().sort_index()
-
-    return zscores, multi_genes_per_cluster
+def cell_type_mapper(cell_type):
+    if ('germ' in cell_type) and 'soma' in cell_type:
+        return 'Germ and Soma'
+    elif 'germ' in cell_type:
+        return 'Germ Only'
+    elif 'soma' in cell_type:
+        return 'Soma Only'
 
 
 if __name__ == '__main__':
