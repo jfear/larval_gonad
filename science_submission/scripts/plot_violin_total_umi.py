830814aa1963843a08c179de2af21b12440f0353
Justin Fear <justin.m.fear@gmail.com>
2019-06-27 17:33:00 -0400

    Cleans up total umi violin plot code.

--- science_submission/scripts/plot_violin_total_umi.py
+++ science_submission/scripts/plot_violin_total_umi.py
@@ -7,22 +7,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from larval_gonad.io import feather_to_cluster_rep_matrix
 
 FNAME = snakemake.input[0]
-
-CLUSTER_ANNOT = snakemake.params.cluster_annot
-CLUSTER_ORDER = snakemake.params.cluster_order
 CLUSTER_COLORS = snakemake.params.cluster_colors
-
 ONAME = snakemake.output[0]
 
 # Debug Settings
 # FNAME = 'output/science_submission/raw_by_cluster_rep.feather'
 # import yaml
 # config = yaml.safe_load(open('config/common.yaml'))
-# CLUSTER_ANNOT = config['cluster_annot']
-# CLUSTER_ORDER = config['cluster_order']
 # CLUSTER_COLORS = yaml.full_load(open('config/colors.yaml'))['clusters']
 
 
@@ -39,16 +32,29 @@ def main():
     plt.style.use('scripts/figure_styles.mplstyle')
     fig, ax = plt.subplots(figsize=(4, 3))
 
-    sns.violinplot('cluster', 'log_UMI', data=df, inner=None, scale='count', order=CLUSTER_ORDER,
-                    palette=CLUSTER_COLORS, linewidth=.5, zorder=0, width=1, ax=ax)
+    sns.violinplot('cluster', 'log_UMI', data=df, 
+        ax=ax,
+        linewidth=.5, 
+        width=.9, 
+        palette=CLUSTER_COLORS, 
+        inner=None, 
+        scale='count', 
+        zorder=0, 
+    )
 
-    sns.boxplot('cluster', 'log_UMI', data=df, order=CLUSTER_ORDER, width=.1, linewidth=.5, ax=ax,
-                zorder=10, showfliers=False, boxprops=dict(zorder=10, facecolor='w'), whiskerprops=dict(zorder=10))
+    sns.boxplot('cluster', 'log_UMI', data=df, 
+        ax=ax, 
+        linewidth=.5, 
+        width=.1, 
+        showfliers=False, 
+        boxprops=dict(zorder=10, facecolor='w'), 
+        whiskerprops=dict(zorder=10),
+        zorder=10, 
+    )
 
     ax.set_axisbelow(True)
     ax.grid(axis='y')
-    ax.set(xlabel='', ylabel='Total UMI Per Cell\n(Log10)')
-    plt.setp(ax.get_xticklabels(), rotation=0)
+    ax.set(xlabel='', ylabel='UMI Per Cell\n(Log10)')
 
     fig.savefig(ONAME, bbox_inches='tight')
 
