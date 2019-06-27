68209e904ab4ed155ccb822b76ad7acd4984fb90
Justin Fear <justin.m.fear@gmail.com>
2019-06-27 16:55:36 -0400

    Y genes boxplot.

--- science_submission/scripts/plot_boxplot_y_genes.py
+++ science_submission/scripts/plot_boxplot_y_genes.py
@@ -2,69 +2,44 @@ import matplotlib
 
 matplotlib.use('Agg')
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-fname = snakemake.input.tpm
-fbgn2chrom = snakemake.input.fbgn2chrom
-clusters = snakemake.input.clusters
-oname = snakemake.output[0]
+FNAME = snakemake.input.tpm
+METADATA = snakemake.input.metadata
+GENE_METADATA = snakemake.input.gene_metadata
 
-annotation = snakemake.params.annotation
-cluster_order = snakemake.params.cluster_order
-cluster_colors = snakemake.params.cluster_colors
+CLUSTER_COLORS = snakemake.params.cluster_colors
+
+ONAME = snakemake.output[0]
+
+# Debug Settings
+# FNAME = 'output/science_submission/tpm_by_cluster_rep.feather'
+# METADATA = 'output/seurat3-cluster-wf/combined_n3_metadata.feather'
+# GENE_METADATA = 'references/gene_annotation_dmel_r6-24.feather'
+# import yaml
+# config = yaml.safe_load(open('config/common.yaml'))
+# CLUSTER_COLORS = yaml.full_load(open('config/colors.yaml'))['clusters']
 
 
 def main():
-    df = get_data()
+    y_fbgns = pd.read_feather(GENE_METADATA).query('FB_chrom == "Y"').FBgn.values
+    tpm = pd.read_feather(FNAME).pipe(lambda df: df[df.FBgn.isin(y_fbgns)])
 
     plt.style.use('scripts/figure_styles.mplstyle')
-    fig, ax = plt.subplots(1, 1, figsize=(2.25, 1))
-
-    sns.boxplot('cluster', 'TPM', data=df, order=cluster_order, palette=cluster_colors, ax=ax, linewidth=.5)
-
-    # Clean up X
-    ax.set_xlabel('')
-
-    # Add additional x annotations
-    yloc = -150
-    pad = yloc * .4
-    ax.text(1.5, yloc - pad, 'Germline', ha='center', va='center', fontsize=6, color=cluster_colors[0])
-    ax.text(5, yloc - pad, 'Somatic\nCyst', ha='center', va='center', fontsize=6, color=cluster_colors[4])
-    ax.text(7.5, yloc - pad, 'Somatic\nOther', ha='center', va='center', fontsize=6, color=cluster_colors[8])
-    ax.text(10, yloc - pad, 'Unknown', ha='center', fontsize=6, color=cluster_colors[-1], va='center')
-    lines = [
-        plt.Line2D([0, 3], [yloc, yloc], color=cluster_colors[0], lw=1.5, clip_on=False),
-        plt.Line2D([4, 6], [yloc, yloc], color=cluster_colors[4], lw=1.5, clip_on=False),
-        plt.Line2D([7, 7.5], [yloc, yloc], color=cluster_colors[7], lw=1.5, clip_on=False),
-        plt.Line2D([7.5, 8], [yloc, yloc], color=cluster_colors[8], lw=1.5, clip_on=False),
-        plt.Line2D([9, 11], [yloc, yloc], color=cluster_colors[-1], lw=1.5, clip_on=False),
-    ]
-
-    for l in lines:
-        ax.add_line(l)
-
-    # Clean up Y axis
-    ax.set_ylabel('Normalized Count (TPM)')
-
-    fig.savefig(oname, bbox_inches='tight')
-
-
-def prop_cells(x):
-    return (x > 0).mean()
-
-
-def get_data():
-    return (
-        pd.read_parquet(fname)
-        .assign(cluster=lambda df: pd.Categorical(df.cluster.map(annotation), ordered=True, categories=cluster_order))
-        .dropna()
-        .join(pd.read_parquet(fbgn2chrom))
-        .query('chrom == "chrY"')
-        .groupby(['rep', 'cluster']).TPM.sum()
-        .reset_index()
+    fig, ax = plt.subplots(1, 1, figsize=(4, 2))
+    sns.boxplot('cluster', 'TPM', data=tpm, 
+        palette=CLUSTER_COLORS, 
+        ax=ax, 
+        linewidth=.5, 
+        showfliers=False,
+        notch=True,
     )
+    ax.set(xlabel='', ylabel='Normalized Count (TPM)')
+
+    fig.savefig(ONAME, bbox_inches='tight')
 
 
 if __name__ == '__main__':
