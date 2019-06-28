0943fc002a23d0197892d8b570b2f4043fcd857f
Justin M. Fear <justin.m.fear@gmail.com>
2019-06-28 11:08:02 -0400

    Adds cell per cluster barplot.

--- science_submission/scripts/plot_barplot_cell_per_cluster.py
+++ science_submission/scripts/plot_barplot_cell_per_cluster.py
@@ -1,69 +1,48 @@
 import matplotlib
 
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-file_name = snakemake.input.dat
-clusters_file_name = snakemake.input.clusters
-oname = snakemake.output[0]
+FILE_NAME = snakemake.input[0]
 
-annotation = snakemake.params.annotation
-cluster_order = snakemake.params.cluster_order
-cluster_colors = snakemake.params.cluster_colors
+CLUSTER_ANNOT = snakemake.params.cluster_annot
+CLUSTER_ORDER = snakemake.params.cluster_order
+
+ONAME = snakemake.output[0]
+
+# Debug Settings
+# FILE_NAME = 'output/seurat3-cluster-wf/combined_n3_metadata.feather'
+# import yaml
+# config = yaml.safe_load(open('config/common.yaml'))
+# CLUSTER_ANNOT = config['cluster_annot']
+# CLUSTER_ORDER = config['cluster_order']
 
 
 def main():
-    df = get_data()
-
-    plt.style.use('scripts/figure_styles.mplstyle')
-    fig, ax = plt.subplots(figsize=(2.25, 1))
-
-    df.plot(kind='bar', width=.9, ax=ax, color=cluster_colors)
-
-    ax.set_axisbelow(True)
-    ax.grid(axis='y')
-
-    # Clean up X axis
-    ax.set_xlabel('')
-    plt.setp(ax.get_xticklabels(), rotation=0)
-
-    # Add additional x annotations
-    yloc = -300
-    pad = yloc * .9
-    ax.text(1.5, yloc + pad, 'Germline', ha='center', va='center', fontsize=6, color=cluster_colors[0])
-    ax.text(5, yloc + pad, 'Somatic\nCyst', ha='center', va='center', fontsize=6, color=cluster_colors[4])
-    ax.text(7.5, yloc + pad, 'Somatic\nOther', ha='center', va='center', fontsize=6, color=cluster_colors[8])
-    ax.text(10, yloc + pad, 'Unknown', ha='center', fontsize=6, color=cluster_colors[-1], va='center')
-    lines = [
-        plt.Line2D([0, 3], [yloc, yloc], color=cluster_colors[0], lw=1.5, clip_on=False),
-        plt.Line2D([4, 6], [yloc, yloc], color=cluster_colors[4], lw=1.5, clip_on=False),
-        plt.Line2D([7, 7.5], [yloc, yloc], color=cluster_colors[7], lw=1.5, clip_on=False),
-        plt.Line2D([7.5, 8], [yloc, yloc], color=cluster_colors[8], lw=1.5, clip_on=False),
-        plt.Line2D([9, 11], [yloc, yloc], color=cluster_colors[-1], lw=1.5, clip_on=False),
-    ]
-
-    for l in lines:
-        ax.add_line(l)
-
-    # Clean up Y axis
-    ax.set_ylabel('Number of Cells')
-
-    ax.set_title('Cells Per Cell Type')
-    fig.savefig(oname, bbox_inches='tight')
-
-
-def get_data():
-    return (
-        pd.read_parquet(file_name)
+    df = (
+        pd.read_feather(FILE_NAME, columns=["cell_id", "cluster"])
+        .assign(cluster=lambda df: df.cluster.cat.rename_categories(CLUSTER_ANNOT))
+        .assign(cluster=lambda df: df.cluster.cat.reorder_categories(CLUSTER_ORDER))
+        .assign(rep=lambda df: df.cell_id.str.extract("(rep\d)", expand=False))
+        .groupby(["cluster", "rep"])
+        .size()
+        .rename("num_cells")
         .reset_index()
-        .assign(cluster=lambda df: pd.Categorical(df.cluster.map(annotation), ordered=True, categories=cluster_order))
-        .assign(rep=lambda df: pd.Categorical(df.rep, ordered=True, categories=['rep1', 'rep2', 'rep3']))
-        .groupby('cluster').number_of_cells.sum()
     )
 
+    plt.style.use("scripts/figure_styles.mplstyle")
+    fig, ax = plt.subplots()
 
-if __name__ == '__main__':
+    sns.barplot("cluster", "num_cells", hue="rep", data=df, ax=ax)
+    ax.set(xlabel="", ylabel="Number of Cells")
+    ax.set_axisbelow(True)
+    ax.grid(axis="y")
+
+    fig.savefig(ONAME, bbox_inches="tight")
+
+
+if __name__ == "__main__":
     main()
