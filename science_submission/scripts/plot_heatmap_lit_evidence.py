2513e0e7df0a93497683009ece3cf3d8885ec773
Justin Fear <justin.m.fear@gmail.com>
2019-07-03 16:01:23 -0400

    Plot showing evidence from the literature.

--- science_submission/scripts/plot_heatmap_lit_evidence.py
+++ science_submission/scripts/plot_heatmap_lit_evidence.py
@@ -22,11 +22,12 @@ ONAME = snakemake.output[0]
 def main():
     fbgn2symbol = pd.read_feather(GENE_METADATA, columns=['FBgn', 'gene_symbol']).set_index('FBgn').to_dict()['gene_symbol']
     evidence = pd.read_csv(EVIDENCE, sep='\t', index_col=0).rename(fbgn2symbol)
+    this_study = evidence.query('References == "This study"').index.tolist()
 
     colors = sns.color_palette('viridis', n_colors=100)
 
     plt.style.use("scripts/paper_1c.mplstyle")
-    fig, ax = plt.subplots(figsize=(4, 8))
+    fig, ax = plt.subplots(figsize=(4, 9))
     sns.heatmap(
         evidence.iloc[:, :-1], 
         cmap=[colors[0], 'lightgray', colors[-1]],
@@ -36,7 +37,6 @@ def main():
         square=True,
         ax=ax
     )
-    plt.setp(ax.get_yticklabels(), fontsize=8, fontfamily='Helvetica')
 
     # Add legend
     low = mpatches.Patch(color=colors[0], label = 'Not Expressed')
@@ -48,11 +48,18 @@ def main():
     ax.set_xlabel("")
     ax.xaxis.set_ticks_position("top")
     ax.xaxis.set_tick_params(pad=0, length=2)
+    plt.setp(ax.get_xticklabels(), fontsize=7, rotation=90)
 
     # Clean up Y axis
     ax.set_ylabel("")
     ax.yaxis.set_tick_params(pad=0.1, length=2)
-    plt.setp(ax.get_yticklabels(), fontstyle="italic", fontfamily="Helvetica", rotation=0, va="center")
+    labels = []
+    for l in ax.get_yticklabels():
+        l.set(fontsize=8, fontfamily='Helvetica', fontstyle='italic')
+        if l.get_text() in this_study:
+            l.set(fontsize=10, fontweight='bold')
+        labels.append(l)
+    ax.set_yticklabels(labels)
 
     fig.savefig(ONAME, bbox_inches="tight")
 
