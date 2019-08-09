13ee1ce27fe13d839c8336ed2a5c796255e5fc2d
Justin M. Fear <justin.m.fear@gmail.com>
2019-08-09 08:41:10 -0400

    Revamped figure.

--- science_submission/scripts/plot_boxplot_demasculinization.py
+++ science_submission/scripts/plot_boxplot_demasculinization.py
@@ -11,24 +11,19 @@ matplotlib.use("Agg")
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 import seaborn as sns
 
 
 BULK_DEG = snakemake.input["bulk"]
-RAW = snakemake.input["raw"]
-NORM = snakemake.input["norm"]
-CELL_ANNOTATION = snakemake.input["cell_annotation"]
+TPM = snakemake.input["tpm"]
 GENE_ANNOTATION = snakemake.input["gene_annotation"]
 
-CLUSTER_ANNOT = snakemake.params["cluster_annot"]
-CLUSTER_ORDER = snakemake.params["cluster_order"]
+CLUSTER_COLORS = snakemake.params["colors"]
 
-PCT_FILE = snakemake.output["pct_cells"]
-NORM_FILE = snakemake.output["norm_expression"]
-
-BOXPLOT_DEFAULTS = dict(
-    showfliers=False, notch=True, order=["X", "2L", "2R", "3L", "3R", "4"], linewdith=0.5
-)
+BULK_DEMASCULINIZATION_FILE = snakemake.output["demas"]
+TESTIS_BIASED_FILE = snakemake.output["testis"]
+OVARY_BIASED_FILE = snakemake.output["ovary"]
 
 # Debug settings
 # import os
@@ -37,111 +32,74 @@ BOXPLOT_DEFAULTS = dict(
 #     print(os.getcwd())
 # except:
 #     pass
-# BULK_DEG = ''
-# RAW = '../../output/cellselection-wf/raw.feather'
-# NORM = '../../output/seurat3-cluster-wf/combined_n3_normalized.feather'
-# CELL_ANNOTATION = "../../output/seurat3-cluster-wf/combined_n3_metadata.feather"
+# BULK_DEG = '../../output/bulk2-rnaseq-wf/deg/bulk_testis_vs_ovary.tsv'
+# TPM = '../../output/seurat3-cluster-wf/tpm_by_cluster_rep.feather'
 # GENE_ANNOTATION = '../../references/gene_annotation_dmel_r6-24.feather'
 # import yaml
-# config = yaml.safe_load(open("../../config/common.yaml"))
-# CLUSTER_ANNOT = config["cluster_annot"]
-# CLUSTER_ORDER = config["cluster_order"]
+# CLUSTER_COLORS = yaml.full_load(open("../../config/colors.yaml"))['clusters']
+
+BARPLOT_DEFAULTS = dict(order=["X", "2L", "2R", "3L", "3R", "4"], color="C0")
+
+BOXPLOT_DEFAULTS = dict(
+    hue="cluster",
+    palette=CLUSTER_COLORS,
+    showfliers=False,
+    notch=True,
+    order=["X", "2L", "2R", "3L", "3R", "4"],
+    linewidth=0.5,
+)
 
 
 def main():
     plt.style.use("scripts/figure_styles.mplstyle")
-    cell2cluster = get_cell_to_cluster(CELL_ANNOTATION)
     fbgn2chrom = get_fbgn2chrom(GENE_ANNOTATION)
-    testis_biased_fbgns = get_testis_biased(BULK_DEG)
 
-    # Proportion of Cells with Expression
-    prop_cells = get_proportion_cells(RAW, cell2cluster, fbgn2chrom, testis_biased_fbgns)
-    prop_cells_subset = filter_by_cluster(prop_cells)
+    bulk = pd.read_csv(BULK_DEG, sep="\t").set_index("FBgn").join(fbgn2chrom)
+    bulk["testis_biased"] = (bulk.padj < 0.01) & (bulk.log2FoldChange > 0)
+    bulk["ovary_biased"] = (bulk.padj < 0.01) & (bulk.log2FoldChange < 0)
 
-    g = sns.FacetGrid(data=prop_cells_subset, row="cluster")
-    g.map(sns.boxplot, "chrom", "prop_cells", **BOXPLOT_DEFAULTS)
-    g.savefig(PCT_FILE, bbox_inches="tight")
+    aggregated = bulk.groupby("chrom")[["testis_biased", "ovary_biased"]].mean().reset_index()
 
-    # Normalized Expression
-    norm_expression = get_norm_cells(NORM, cell2cluster, fbgn2chrom, testis_biased_fbgns)
-    norm_expression_subset = filter_by_cluster(norm_expression)
-    avg_norm_expression = (
-        norm_expression_subset.groupby(["chrom", "FBgn", "cluster"])
-        .norm_expression.mean()
-        .reset_index()
-    )
+    tpm_testis = get_tpm_subset(bulk[bulk.testis_biased].index.tolist(), fbgn2chrom)
+    tpm_ovary = get_tpm_subset(bulk[bulk.ovary_biased].index.tolist(), fbgn2chrom)
 
-    g = sns.FacetGrid(data=avg_norm_expression, row="cluster")
-    g.map(sns.boxplot, "chrom", "norm_expression", **BOXPLOT_DEFAULTS)
-    g.savefig(NORM_FILE, bbox_inches="tight")
+    fig = plt.figure(figsize=(8, 12))
+    gs = GridSpec(3, 4, width_ratios=[1, .2, 1, .2])
+    ax_testis_biased = fig.add_subplot(gs[0, 0:2])
+    ax_ovary_biased = fig.add_subplot(gs[0, 2:])
+    ax_testis_cluster = fig.add_subplot(gs[1, :-1])
+    ax_ovary_cluster = fig.add_subplot(gs[2, :-1])
 
+    sns.barplot("chrom", "testis_biased", data=aggregated, ax=ax_testis_biased, **BARPLOT_DEFAULTS)
+    ax_testis_biased.set(ylim=(0, 1), ylabel="Percent Genes", xlabel="", title="Bulk\nTestis Biased")
 
-def get_testis_biased(fname, alpha=0.01):
-    # TODO: Check filtering
-    return (
-        pd.read_feather(fname).query(f"adj_p-val <= {alpha} & avg_logFC > 0").FBgn.values.tolist()
-    )
+    sns.barplot("chrom", "ovary_biased", data=aggregated, ax=ax_ovary_biased, **BARPLOT_DEFAULTS)
+    ax_ovary_biased.set(ylim=(0, 1), ylabel="", xlabel="", yticklabels=[], title="Bulk\nOvary Biased")
 
+    sns.boxplot("chrom", "TPM", data=tpm_testis, ax=ax_testis_cluster, **BOXPLOT_DEFAULTS)
+    ax_testis_cluster.set(ylabel="log10(TPM + 1)", xlabel="", title="Testis Biased Genes")
+    ax_testis_cluster.legend(loc="upper left", bbox_to_anchor=[1, 1])
 
-def get_cell_to_cluster(fname):
-    return (
-        pd.read_feather(fname)
-        .set_index("cell_id")
-        .assign(
-            cluster=lambda df: pd.Categorical(
-                df.cluster.map(CLUSTER_ANNOT), ordered=True, categories=CLUSTER_ORDER
-            )
-        )
-        .cluster
-    )
-
-
-def get_proportion_cells(fname, cell2cluster, fbgn2chrom, target_fbgns):
-    return (
-        pd.read_feather(fname)
-        .set_index("FBgn")
-        .reindex(target_fbgns)
-        .dropna()
-        .T.pipe(lambda x: x > 0)
-        .join(cell2cluster)
-        .groupby("cluster")
-        .mean()
-        .T.rename_axis("FBgn")
-        .stack()
-        .rename("prop_cells")
-        .reset_index(level=-1)
-        .join(fbgn2chrom, how="inner")
-    )
-
-
-def get_norm_cells(fname, cell2cluster, fbgn2chrom, target_fbgns):
-    return (
-        pd.read_feather(fname)
-        .set_index("FBgn")
-        .drop("gene_symbol", axis=1)
-        .rename_axis("cell_id", axis=1)
-        .reindex(target_fbgns)
-        .dropna()
-        .stack()
-        .rename("norm_expression")
-        .reset_index(level=0)
-        .join(cell2cluster)
-        .set_index("FBgn")
-        .join(fbgn2chrom, how="inner")
-    )
+    sns.boxplot("chrom", "TPM", data=tpm_ovary, ax=ax_ovary_cluster, **BOXPLOT_DEFAULTS)
+    ax_ovary_cluster.set(ylabel="log10(TPM + 1)", xlabel="", title="Ovary Biased Genes")
+    ax_ovary_cluster.legend(loc="upper left", bbox_to_anchor=[1, 1])
 
 
 def get_fbgn2chrom(fname):
     return pd.read_feather(fname).set_index("FBgn").FB_chrom.rename("chrom")
 
 
-def filter_by_cluster(df):
-    _df = df.query("cluster == ['SP', 'EPS', 'PS1', 'PS2', 'PS3', 'ECY', 'CY1', 'CY2']").copy()
-    clusters = _df.cluster.astype(str).replace(
-        dict(PS1="PSs", PS2="PSs", PS3="PSs", ECY="CYs", CY1="CYs", CY2="CYs")
+def get_tpm_subset(fbgns, fbgn2chrom):
+    return (
+        pd.read_feather(TPM)
+        .groupby(["FBgn", "cluster"])
+        .TPM.median()
+        .pipe(lambda x: np.log10(x + 1))
+        .to_frame()
+        .reset_index()
+        .query(f"FBgn == {fbgns}")
+        .merge(fbgn2chrom, left_on="FBgn", right_index=True)
     )
-    _df["cluster"] = pd.Categorical(clusters, ordered=True, categories=["SP", "EPS", "PSs", "CYs"])
-    return _df
 
 
 if __name__ == "__main__":
