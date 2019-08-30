f67ac203c08232e7e6c507de74ebf984e81ee8e4
Justin M. Fear <justin.m.fear@gmail.com>
2019-08-30 18:18:53 -0400

    Gets YOgn metdata from gtf.
    Parses GTF and pulls out major metadata for the
    YOgn level and makes table for each species.

---
+++ expression-atlas-wf/scripts/yogn_to_metadata.py
@@ -0,0 +1,46 @@
+"""Parse GTF file and aggregate gene metadata.
+
+Capture key metadata from GTF file:
+    * chromosome/scaffold
+    * start
+    * end
+    * strand
+"""
+import pandas as pd
+
+from larval_gonad.io import GffRow
+from larval_gonad.debug import snakemake_debug
+
+
+def main():
+    res = [
+        (ts.parsed_attributes["gene_id"], ts.seqid, ts.start, ts.end, ts.strand)
+        for ts in gff_reader(snakemake.input[0])
+    ]
+
+    df = pd.DataFrame(res, columns=["YOgn", "chrom", "start", "end", "strand"])
+
+    yo2metdata = df.groupby("YOgn").agg(
+        {"chrom": "first", "start": min, "end": max, "strand": "first"}
+    )
+
+    yo2metdata.reset_index().to_feather(snakemake.output[0])
+
+
+def gff_reader(file_name):
+    with open(file_name) as fh:
+        for row in fh:
+            gff = GffRow(row)
+            if gff.type == "transcript":
+                yield gff
+
+
+if __name__ == "__main__":
+    DEBUG = False
+
+    if DEBUG:
+        snakemake = snakemake_debug(
+            workdir="expression-atlas-wf", input="../output/expression-atlas-wf/GTF/dana.gtf"
+        )
+
+    main()
