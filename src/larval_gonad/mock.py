8739350d69f84f3c0ad04f497880d023335209b0
Justin M. Fear <justin.m.fear@gmail.com>
2019-08-30 15:20:37 -0400

    simple mock of the snakemake object.

---
+++ src/larval_gonad/mock.py
@@ -0,0 +1,24 @@
+from snakemake.io import Namedlist, InputFiles, OutputFiles, Wildcards, Params, Log
+
+
+class MockSnake:
+    """Mock the snakemake class used inside of python scripts."""
+
+    def __init__(self, input=None, output=None, params=None, wildcards=None, log="", config=None):
+        self.input = InputFiles(self.make_namedlist(input))
+        self.output = OutputFiles(self.make_namedlist(output))
+        self.params = Params(self.make_namedlist(params))
+        self.wildcards = Wildcards(self.make_namedlist(wildcards))
+        self.log = Log(log)
+        self.config = config or {}
+        self.rulename = "mock"
+
+    def make_namedlist(self, item):
+        if isinstance(item, list):
+            return Namedlist(item)
+        elif isinstance(item, dict):
+            return Namedlist(fromdict={k: self.make_namedlist(v) for k, v in item.items()})
+        elif isinstance(item, str):
+            return Namedlist([item])
+
+        return Namedlist([])
