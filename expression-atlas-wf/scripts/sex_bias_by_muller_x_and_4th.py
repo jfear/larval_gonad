bb16a9de79ea8311a58371aba39ea81bbed1933f
Justin M. Fear <justin.m.fear@gmail.com>
2019-09-02 15:08:27 -0400

    Adds more focused enrichment test.
    We are focused on the X and 4th. Instead of performing
    enrichment analyais on all muller elements seperately,
    I wanted to aggregate the autosomes and focus on the
    X and 4th.

---
+++ expression-atlas-wf/scripts/sex_bias_by_muller_x_and_4th.py
@@ -0,0 +1,111 @@
+"""Summarize sex biased expression by Muller element.
+
+1. Read differential sex-biased expression results.
+2. Read in Muller element mapping.
+3. Tabulate the number of genes Up/Down/NS per Muller element.
+5. Calculate proportions of Up/Down/NS per Muller element for plotting.
+4. Calculate enrichment using chi-sq with post-hoc test.
+
+"""
+import os
+import pandas as pd
+
+from larval_gonad.io import pickle_load, shelve_dump
+from larval_gonad.stats import run_chisq
+
+
+def main():
+    deg = read_deg(snakemake.input.deg, float(snakemake.params.alpha))
+    bias_counts_by_muller = count_by_muller(snakemake.input.muller, deg)
+    agg_counts_by_muller = aggregate_autosomes(bias_counts_by_muller)
+    muller_proportions = calc_proprotions(bias_counts_by_muller)
+    enrichment = enrichment_analysis(agg_counts_by_muller)
+
+    # Pull out q-values with locations for easy plotting.
+    male_qval = pull_out_qval(enrichment, muller_proportions, "male")
+    female_qval = pull_out_qval(enrichment, muller_proportions, "female")
+
+    shelve_dump(
+        snakemake.output[0], data=muller_proportions, male_qval=male_qval, female_qval=female_qval
+    )
+
+
+def read_deg(file_deg: str, alpha: float = 0.01) -> pd.DataFrame:
+    """Read deg and make bias flags.
+
+    Make a flag for male, female, and NS.
+
+    """
+    deg = pd.read_csv(file_deg, sep="\t").set_index("YOgn").fillna(1).assign(bias="NS")
+    sig_mask = deg.padj <= alpha
+    deg.loc[sig_mask & (deg.log2FoldChange > 0), "bias"] = "male"
+    deg.loc[sig_mask & (deg.log2FoldChange < 0), "bias"] = "female"
+
+    return deg
+
+
+def count_by_muller(file_muller: str, deg: pd.DataFrame) -> pd.DataFrame:
+    # Read muller element
+    yo2muller = pd.Series(pickle_load(file_muller), name="muller").rename_axis("YOgn")
+
+    # Merge and count Male/Female/NS by muller
+    return (
+        deg.join(yo2muller)
+        .groupby("muller")
+        .bias.value_counts()
+        .unstack()
+        .fillna(0)
+        .loc[["A", "B", "C", "D", "E", "F"], ["male", "female", "NS"]]
+    )
+
+
+def aggregate_autosomes(counts_by_muller):
+    """Add autosome counts together."""
+    non_autosome = counts_by_muller.reindex(["A", "F"])
+    autosomes = (
+        counts_by_muller.reindex(["B", "C", "D", "E"]).sum().rename("autosomes").to_frame().T
+    )
+    return pd.concat([non_autosome, autosomes])
+
+
+def calc_proprotions(counts_by_muller: pd.DataFrame) -> pd.DataFrame:
+    col_order = ["male", "NS", "female"]
+    return counts_by_muller.pipe(lambda x: x.div(x.sum(axis=1), axis=0)).loc[:, col_order]
+
+
+def enrichment_analysis(counts_by_muller: pd.DataFrame) -> pd.DataFrame:
+    return (
+        run_chisq(counts_by_muller.T)
+        .loc[(["male", "female"], "fdr q-value"), :]
+        .droplevel("type")
+        .T
+    )
+
+
+def pull_out_qval(enrichment: pd.DataFrame, proportions: pd.DataFrame, sex: str):
+    df = (
+        enrichment[sex]
+        .rename("q-value")
+        .to_frame()
+        .join(proportions[sex].rename("y"))
+        .reindex(["A", "F"])
+    )
+    df["x"] = 0
+    df.loc["F", "x"] = 5
+    return df
+
+
+if __name__ == "__main__":
+    if os.getenv("SNAKE_DEBUG", False):
+        from larval_gonad.debug import snakemake_debug
+
+        snakemake = snakemake_debug(
+            workdir="expression-atlas-wf",
+            input={
+                "muller": "../output/expression-atlas-wf/YOgn_to_muller/dmel.pkl",
+                "deg": "../output/expression-atlas-wf/sex_biased_expression/orgR_AC.tsv",
+            },
+            params=dict(alpha=0.01),
+        )
+
+    main()
