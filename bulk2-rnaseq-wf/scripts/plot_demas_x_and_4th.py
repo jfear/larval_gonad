66ed17a8caa7a91a19e982518526f446cf89d642
Justin M. Fear <justin.m.fear@gmail.com>
2019-09-04 08:21:36 -0400

    Refactor out the plot to general demas plot.

--- bulk2-rnaseq-wf/scripts/plot_demas_x_and_4th.py
+++ bulk2-rnaseq-wf/scripts/plot_demas_x_and_4th.py
@@ -10,50 +10,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from larval_gonad.io import shelve_load
-from larval_gonad.plotting import format_pval
+from larval_gonad.plotting import demasculinization, add_pvals
 
 
 def main():
-    df, male, female = get_data(snakemake.input[0])
-
-    fig, ax = plt.subplots(figsize=plt.figaspect(2))
-    plot(df, ax, f"larval (GO)", snakemake.params.colors)
-    add_pval(male, ax)
-    add_pval(female, ax)
-
-    plt.savefig(snakemake.output[0])
-
-
-def get_data(file_name: str):
-    """Loads shelve and get data ready for plotting"""
-    db = shelve_load(file_name)
+    db = shelve_load(snakemake.input[0])
     df = db["data"]
     male = db["male_qval"].fillna(1)  # If p-vals are NaN set to 1
-    female = db["female_qval"].fillna(1)
-
     # Change female Y location to come from top.
+    female = db["female_qval"].fillna(1)
     female.y = 1 - female.y
 
-    return df, male, female
-
-
-def plot(df, ax, title, colors):
-    """Make main plot"""
-    df.plot.bar(
-        stacked=True, ax=ax, color=colors, width=0.9, edgecolor="k", linewidth=0.2
-    )
-    plt.setp(ax.get_xticklabels(), rotation=0)
-    ax.set(title=title, xlabel="", ylim=(0, 1))
-    ax.legend_ = None
-    sns.despine(ax=ax, left=True, bottom=True)
-    return ax
-
-
-def add_pval(df, ax):
-    """Add *** to plot"""
-    for _, row in df.iterrows():
-        format_pval(ax, row.x, row.y, row["q-value"], va="center", fontsize=10)
-    return ax
+    fig, ax = plt.subplots(figsize=plt.figaspect(2))
+    demasculinization(df, ax=ax, title=f"larval (GO)", color=snakemake.params.colors)
+    add_pvals(male.x.values, male.y.values, male['q-value'].values, ax)
+    add_pvals(female.x.values, female.y.values, female['q-value'].values, ax)
+    plt.savefig(snakemake.output[0])
 
 
 if __name__ == "__main__":
@@ -62,10 +34,8 @@ if __name__ == "__main__":
 
         snakemake = snakemake_debug(
             workdir="bulk2-rnaseq-wf",
-            input='../output/bulk2-rnaseq-wf/testis_bias_by_muller_x_and_4th.dat',
-            params=dict(
-                colors=["blue", "lightgray", "red"],
-            ),
+            input="../output/bulk2-rnaseq-wf/testis_bias_by_muller_x_and_4th.dat",
+            params=dict(colors=["blue", "lightgray", "red"]),
         )
         plt.style.use("../config/figure_styles.mplstyle")
 
--- expression-atlas-wf/scripts/plot_demas_x_and_4th.py
+++ expression-atlas-wf/scripts/plot_demas_x_and_4th.py
@@ -10,53 +10,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from larval_gonad.io import shelve_load
-from larval_gonad.plotting import format_pval
+from larval_gonad.plotting import demasculinization, add_pvals
 
 
 def main():
     species = snakemake.wildcards.species
     tissue = snakemake.wildcards.tissue
+    db = shelve_load(snakemake.input[0])
 
-    df, male, female = get_data(snakemake.input[0])
-
-    fig, ax = plt.subplots(figsize=plt.figaspect(2))
-    plot(df, ax, f"{species} ({tissue})", snakemake.params.colors)
-    add_pval(male, ax)
-    add_pval(female, ax)
-
-    plt.savefig(snakemake.output[0])
-
-
-def get_data(file_name: str):
-    """Loads shelve and get data ready for plotting"""
-    db = shelve_load(file_name)
     df = db["data"]
     male = db["male_qval"].fillna(1)  # If p-vals are NaN set to 1
-    female = db["female_qval"].fillna(1)
-
     # Change female Y location to come from top.
+    female = db["female_qval"].fillna(1)
     female.y = 1 - female.y
 
-    return df, male, female
-
-
-def plot(df, ax, title, colors):
-    """Make main plot"""
-    df.plot.bar(
-        stacked=True, ax=ax, color=colors, width=0.9, edgecolor="k", linewidth=0.2
-    )
-    plt.setp(ax.get_xticklabels(), rotation=0)
-    ax.set(title=title, xlabel="", ylim=(0, 1))
-    ax.legend_ = None
-    sns.despine(ax=ax, left=True, bottom=True)
-    return ax
-
-
-def add_pval(df, ax):
-    """Add *** to plot"""
-    for _, row in df.iterrows():
-        format_pval(ax, row.x, row.y, row["q-value"], va="center", fontsize=10)
-    return ax
+    fig, ax = plt.subplots(figsize=plt.figaspect(2))
+    demasculinization(df, ax=ax, title=f"{species} ({tissue})", color=snakemake.params.colors)
+    add_pvals(male.x.values, male.y.values, male["q-value"].values, ax)
+    add_pvals(female.x.values, female.y.values, female["q-value"].values, ax)
+    plt.savefig(snakemake.output[0])
 
 
 if __name__ == "__main__":
@@ -65,11 +37,9 @@ if __name__ == "__main__":
 
         snakemake = snakemake_debug(
             workdir="science_submission",
-            input='../output/expression-atlas-wf/sex_bias_by_muller_x_and_4th/dana_AC.dat',
-            params=dict(
-                colors=["blue", "lightgray", "red"],
-            ),
-            wildcards=dict(species="dana", tissue="AC")
+            input="../output/expression-atlas-wf/sex_bias_by_muller_x_and_4th/dana_AC.dat",
+            params=dict(colors=["blue", "lightgray", "red"]),
+            wildcards=dict(species="dana", tissue="AC"),
         )
         plt.style.use("../config/figure_styles.mplstyle")
 
