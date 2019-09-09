e2cf3728d2fad6cd28800a5859ce76f8494fb41c
Justin M. Fear <justin.m.fear@gmail.com>
2019-09-09 09:07:00 -0400

    Reclusters and outputs for germ line.
    Reclusters just the germline cells and then
    outputs umap, clusters, and biomarkers.

--- germ-trajectory-wf/scripts/seurat3_recluster.R
+++ germ-trajectory-wf/scripts/seurat3_recluster.R
@@ -1,5 +1,7 @@
-library(seurat3)
+library(Seurat)
 library(dplyr)
+library(tibble)
+library(feather)
 library(ggplot2)
 
 if (Sys.getenv("SNAKE_DEBUG", FALSE)) {
@@ -7,7 +9,6 @@ if (Sys.getenv("SNAKE_DEBUG", FALSE)) {
     snakemake <- snakemake_debug(
         input = list(
             robj = "../output/seurat3-cluster-wf/combined_n3.Robj",
-            lit_genes = "../config/literature_genes.yaml",
             gene_annot = "../references/gene_annotation_dmel_r6-26.feather"
         ),
         params = list(germ_clusters = c(6, 4, 2, 0))
@@ -15,38 +16,45 @@ if (Sys.getenv("SNAKE_DEBUG", FALSE)) {
 }
 
 # Load Metadata
-lit_genes <- yaml::yaml.load_file(snakemake@input[["lit_genes"]])
-fbgn2symbol <- feather::read_feather(snakemake@input[["gene_annot"]], columns = c("FBgn", "gene_symbol"))
+fbgn2symbol <- read_feather(snakemake@input[["gene_annot"]], columns = c("FBgn", "gene_symbol"))
 
 # Load combined robj
 load(snakemake@input[["robj"]])
 
-# Subset out the germline clusters
+# Subset out the germline clusters and clear metadata
 sobj <- subset(combined, ident = snakemake@params[["germ_clusters"]])
+sobj@meta.data <- sobj@meta.data %>%
+    select(orig.ident, nCount_RNA, nFeature_RNA, sample)
 
-# Re-process with subset
+# Re-process the subset
 sobj <- RunPCA(sobj, npcs = 30, features = VariableFeatures(sobj), verbose = FALSE)
 sobj <- RunUMAP(sobj, reduction = "pca", dims = 1:9)
 sobj <- FindNeighbors(sobj, reduction = "pca", dims = 1:9, verbose = FALSE)
-sobj <- FindClusters(sobj, resolution = 0.2, verbose = FALSE)
+sobj <- FindClusters(sobj, resolution = 0.4, verbose = FALSE)
 
-# Plot new clusters
-DimPlot(sobj, reduction = "umap")
-
-# Plot gene sets
-gonia <- fbgn2symbol %>%
-    filter(FBgn %in% lit_genes[["gonia"]]) %>%
-    pull(gene_symbol)
-FeaturePlot(sobj, features = gonia)
-
-ps <- fbgn2symbol %>%
-    filter(FBgn %in% lit_genes[["spermatocytes"]]) %>%
-    pull(gene_symbol)
-FeaturePlot(sobj, features = ps)
-
-# Look at new biomarkers
+# Find new biomarkers
 biomarkers <- FindAllMarkers(sobj, only.pos = TRUE, verbose = FALSE)
-biomarkers %>%
-    filter(cluster == 0) %>%
-    arrange(gene) %>%
-    pull(gene)
+
+# Save out files
+## UMAP
+umap <- as_tibble(
+    as.data.frame(Embeddings(sobj, reduction = "umap")) %>%
+        rownames_to_column("cell_id")
+)
+write_feather(umap, path = snakemake@output[["umap"]])
+
+## Clusters
+metadata <- as_tibble(
+    sobj@meta.data %>%
+        rownames_to_column("cell_id") %>%
+        mutate(cluster = seurat_clusters) %>%
+        select(cell_id, orig.ident, nCount_RNA, nFeature_RNA, sample, cluster)
+)
+write_feather(metadata, path = snakemake@output[["metadata"]])
+
+## Biomarkers
+biomarkers <- as_tibble(biomarkers) %>%
+    rename(gene_symbol = gene) %>%
+    left_join(fbgn2symbol) %>%
+    select(FBgn, gene_symbol, cluster, everything())
+write_feather(biomarkers, path = snakemake@output[["biomarkers"]])
