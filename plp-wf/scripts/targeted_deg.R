87442721066d131b87c7c713991dc449a93fe756
Justin M. Fear <justin.m.fear@gmail.com>
2019-10-11 10:06:32 -0400

    Adds additional comparions and outputs tsne

--- plp-wf/scripts/targeted_deg.R
+++ plp-wf/scripts/targeted_deg.R
@@ -13,11 +13,12 @@ fbgn2symbol <- read_feather(
 # Load Combined Data
 load("../../output/seurat3-cluster-wf/combined_n3.Robj")
 
-# Run PCA
-pca <- combined@reductions$pca@cell.embeddings[, 1:2] %>%
+# Run tSNE
+combined <- RunTSNE(combined, reduction = "pca", dims = 1:9)
+tsne <- combined@reductions$tsne@cell.embeddings[, 1:2] %>%
     as.data.frame() %>%
     rownames_to_column("cell_id")
-write.table(pca, "../../output/plp-wf/pca.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
+write.table(tsne, "../../output/plp-wf/tsne.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
 
 # Trial List
 trial_fbgns <- read.table(
