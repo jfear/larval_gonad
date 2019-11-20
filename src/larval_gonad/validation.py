675620493f34f58d2e5cffcaaf161d65ac568665
Justin Fear <justin.m.fear@gmail.com>
2019-11-20 15:42:01 -0500

    Working version of gene validator.
    Initial version of the gene validator.

--- src/larval_gonad/validation.py
+++ src/larval_gonad/validation.py
@@ -11,14 +11,14 @@ class GeneValidator(object):
     germ_lineage = config["germ"]
     cyst_lineage = config["cyst"]
 
-    def __init__(self, FBgn, experiment_type, lit_gene, lit_missing, biomarker, zscore):
+    def __init__(self, FBgn, lit_gene, lit_missing, biomarker, zscore, flag_protein=False):
         self.fbgn = FBgn
-        self.exp_type = experiment_type
         self.lit_gene = lit_gene
         self.lit_missing = lit_missing
         self.biomarker = biomarker
         self.non_missing_biomarker = self.biomarker - self.lit_missing
         self.zscore = zscore
+        self.flag_protein = flag_protein
         self.non_missing_zscore = self.zscore - self.lit_missing
         self.score = None
         self.validate()
@@ -29,31 +29,33 @@ class GeneValidator(object):
         elif self.lvl2():
             return
 
-        if self.exp_type == "ISH":
-            if self.lvl3_ish():
+        if self.flag_protein:
+            if self.lvl3_protein():
                 return
-            elif self.lvl4_ish():
+            elif self.lvl4_protein():
                 return
-
-        if self.exp_type == "IHC":
-            if self.lvl3_ihc():
+        else:
+            if self.lvl3_rna():
                 return
-            elif self.lvl4_ihc():
+            elif self.lvl4_rna():
                 return
 
     def lvl1(self):
-        """Lit and Biomarkers exact match"""
+        """{Lit} == {Biomarkers}"""
         if self.lit_gene == self.non_missing_biomarker:
             self.score = 4
             return True
 
     def lvl2(self):
-        """Lit and upper zscore quantail exact match"""
+        """{Lit} == {q75(Zscores)}"""
         if self.lit_gene == self.non_missing_zscore:
             self.score = 3
             return True
 
-    def lvl3_ish(self):
+    def lvl3_rna(self):
+        """({Lit} subset {Germ} & {Biomarkers} subset {Germ}) or
+           ({Lit} subset {Cyst} & {Biomarkers} subset {Cyst})
+        """
         if (
             self.lit_gene.issubset(self.germ_lineage)
             & self.biomarker.issubset(self.germ_lineage)
@@ -66,20 +68,31 @@ class GeneValidator(object):
             self.score = 2
             return True
 
-    def lvl3_ihc(self):
+    def lvl3_protein(self):
+        """Gene expression proceeds in the lineage."""
         if (
             self.lit_gene.issubset(self.germ_lineage)
             & self.biomarker.issubset(self.germ_lineage)
             & bool(self.biomarker)
-        ) | (
+        ):
+            protein_idx = max([self.germ_lineage.index(x) + 1 for x in self.lit_gene])
+            if self.proceeds(protein_idx, self.biomarker, self.germ_lineage):
+                self.score = 2
+                return True
+        elif (
             self.lit_gene.issubset(self.cyst_lineage)
             & self.biomarker.issubset(self.cyst_lineage)
             & bool(self.biomarker)
         ):
-            self.score = 2
-            return True
+            protein_idx = max([self.cyst_lineage.index(x) + 1 for x in self.lit_gene])
+            if self.proceeds(protein_idx, self.biomarker, self.cyst_lineage):
+                self.score = 2
+                return True
 
-    def lvl4_ish(self):
+    def lvl4_rna(self):
+        """({Lit} subset {Germ} & {q75(Zscores)} subset {Germ}) or
+           ({Lit} subset {Cyst} & {q75(Zscores)} subset {Cyst})
+        """
         if (
             self.lit_gene.issubset(self.germ_lineage)
             & self.zscore.issubset(self.germ_lineage)
@@ -92,18 +105,34 @@ class GeneValidator(object):
             self.score = 1
             return True
 
-    def lvl4_ihc(self):
+    def lvl4_protein(self):
+        """Gene expression proceeds in the lineage."""
         if (
             self.lit_gene.issubset(self.germ_lineage)
             & self.zscore.issubset(self.germ_lineage)
             & bool(self.zscore)
-        ) | (
+        ):
+            protein_idx = max([self.germ_lineage.index(x) + 1 for x in self.lit_gene])
+            if self.proceeds(protein_idx, self.zscore, self.germ_lineage):
+                self.score = 1
+                return True
+        elif (
             self.lit_gene.issubset(self.cyst_lineage)
             & self.zscore.issubset(self.cyst_lineage)
             & bool(self.zscore)
         ):
-            self.score = 1
-            return True
+            protein_idx = max([self.cyst_lineage.index(x) + 1 for x in self.lit_gene])
+            if self.proceeds(protein_idx, self.zscore, self.cyst_lineage):
+                self.score = 1
+                return True
+
+    def proceeds(self, protein_idx, cell_types, lineage):
+        """Check if any cell types proceed the protein index."""
+        for cell_type in cell_types:
+            rna_idx = lineage.index(cell_type) + 1
+            _diff = protein_idx - rna_idx
+            if _diff in [0, 1]:
+                return True
 
     def __str__(self):
         return f"{self.fbgn}\t{self.lit_gene}\t{self.biomarker}\t{self.zscore}\t{self.score}"
--- src/tests/test_validation.py
+++ src/tests/test_validation.py
@@ -4,31 +4,49 @@ from larval_gonad.validation import GeneValidator
 
 
 @pytest.mark.parametrize(
-    "fbgn,experiment_type,lit,missing,biomarkers,zscores,expected_score",
+    "lit,missing,biomarkers,zscores,flag_protein,expected_score",
     [
         # Fours: Lit Gene matches BioMarkers
-        ("gene1", "ISH", {"G"}, set(), {"G"}, set(), 4),
-        ("gene1", "IHC", {"G"}, set(), {"G"}, set(), 4),
-        ("gene1", "ISH", {"G"}, set(), {"G"}, {"G"}, 4),
-        ("gene1", "ISH", {"G"}, set(), {"G"}, {"G", "EPS"}, 4),
-        ("gene1", "ISH", {"G", "EPS"}, set(), {"G", "EPS"}, set(), 4),
-        ("gene1", "ISH", {"G", "EPS"}, set(), {"G", "EPS"}, {"C1"}, 4),
-        ("gene1", "ISH", {"G", "EPS"}, {"MPS", "EC"}, {"G", "EPS", "MPS"}, set(), 4),
+        ## in situ
+        ({"G"}, set(), {"G"}, set(), False, 4),
+        ({"G"}, set(), {"G"}, {"G"}, False, 4),
+        ({"G"}, set(), {"G"}, {"G", "EPS"}, False, 4),
+        ({"G", "EPS"}, set(), {"G", "EPS"}, set(), False, 4),
+        ({"G", "EPS"}, set(), {"G", "EPS"}, {"C1"}, False, 4),
+        ({"G", "EPS"}, {"MPS", "EC"}, {"G", "EPS", "MPS"}, set(), False, 4),
+        ## protein expression
+        ({"G"}, set(), {"G"}, set(), True, 4),
+        ({"G", "EPS"}, set(), {"G", "EPS"}, set(), True, 4),
         # Threes: Lit Gene matches Upper Quantile of Zscores
-        ("gene1", "ISH", {"G"}, set(), set(), {"G"}, 3),
-        ("gene1", "IHC", {"G"}, set(), set(), {"G"}, 3),
-        ("gene1", "ISH", {"G", "EPS"}, set(), set(), {"G", "EPS"}, 3),
-        ("gene1", "ISH", {"G", "EPS"}, {"MPS"}, set(), {"G", "EPS", "MPS"}, 3),
+        ## in situ
+        ({"G"}, set(), set(), {"G"}, False, 3),
+        ({"G", "EPS"}, set(), set(), {"G", "EPS"}, False, 3),
+        ({"G", "EPS"}, {"MPS"}, set(), {"G", "EPS", "MPS"}, False, 3),
+        ## protein expression
+        ({"G"}, set(), set(), {"G"}, True, 3),
+        ({"G", "EPS"}, set(), set(), {"G", "EPS"}, True, 3),
         # Twos: Lit Gene matches Lineage Biomarkers
-        ("gene1", "ISH", {"G", "EPS"}, set(), {"G", "EPS", "MPS"}, set(), 2),
-        ("gene1", "ISH", {"C1", "C4"}, set(), {"C1", "C2", "C3"}, set(), 2),
+        ## in situ
+        ({"G", "EPS"}, set(), {"G", "EPS", "MPS"}, set(), False, 2),
+        ({"C1", "C4"}, set(), {"C1", "C2", "C3"}, set(), False, 2),
+        ## protein expression
+        ({"EPS"}, set(), {"G"}, set(), True, 2),
+        ({"C2"}, set(), {"C1"}, set(), True, 2),
+        ({"C1", "C2"}, set(), {"C1", "C2", "C3"}, set(), True, 2),
         # Ones: Lit Gene matches Lineage Zscores
-        ("gene1", "ISH", {"G", "EPS"}, set(), set(), {"G", "EPS", "MPS"}, 1),
-        ("gene1", "ISH", {"C1", "C4"}, set(), set(), {"C1", "C2", "C3"}, 1),
+        ## in situ
+        ({"G", "EPS"}, set(), set(), {"G", "EPS", "MPS"}, False, 1),
+        ({"C1", "C4"}, set(), set(), {"C1", "C2", "C3"}, False, 1),
+        ## protein expression
+        ({"G", "EPS"}, set(), set(), {"G", "EPS", "MPS"}, True, 1),
         # No class
-        ("gene1", "ISH", {"G"}, set(), set(), set(), None),
+        ## in situ
+        ({"G"}, set(), set(), set(), False, None),
+        ## protein expression
+        ({"G"}, set(), set(), set(), True, None),
+        ({"MPS"}, set(), {"G"}, set(), True, None),
     ],
 )
-def test_GeneValidator(fbgn, experiment_type, lit, missing, biomarkers, zscores, expected_score):
-    gene = GeneValidator(fbgn, experiment_type, lit, missing, biomarkers, zscores)
+def test_GeneValidator(lit, missing, biomarkers, zscores, flag_protein, expected_score):
+    gene = GeneValidator("gene1", lit, missing, biomarkers, zscores, flag_protein)
     assert gene.score == expected_score
