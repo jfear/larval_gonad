0b935f0093ffbdf8ecb65c0bd6f1acccf4c0071c
Justin Fear <justin.m.fear@gmail.com>
2019-11-21 15:23:48 -0500

    Tweaked logic.

--- src/larval_gonad/validation.py
+++ src/larval_gonad/validation.py
@@ -2,6 +2,8 @@
 from pathlib import Path
 from itertools import product
 
+import numpy as np
+
 from larval_gonad.config import read_config
 
 
@@ -26,33 +28,60 @@ class GeneValidator(object):
 
     def validate(self):
         if self.lit_gene == set():
-            return 
-
-        if self.lvl1():
-            return
-        elif self.lvl2():
             return
 
         if self.flag_protein:
-            if self.lvl3_protein():
+            if self.lvl1_protein():
+                return
+            elif self.lvl2_protein():
+                return
+            elif self.lvl3_protein():
                 return
             elif self.lvl4_protein():
                 return
         else:
-            if self.lvl3_rna():
+            if self.lvl1_rna():
+                return
+            elif self.lvl2_rna():
+                return
+            elif self.lvl3_rna():
                 return
             elif self.lvl4_rna():
                 return
 
-    def lvl1(self):
+    def lvl1_rna(self):
         """{Lit} == {Biomarkers}"""
         if self.lit_gene == self.non_missing_biomarker:
             self.score = 4
             return True
 
-    def lvl2(self):
+    def lvl1_protein(self):
+        """{Lit} == {Biomarkers} or {Biomarkers} less than"""
+        if self.lit_gene == self.non_missing_biomarker:
+            self.score = 4
+            return True
+        elif self.all_proceeds(self.lit_gene, self.biomarker, self.germ_lineage):
+            self.score = 4
+            return True
+        elif self.all_proceeds(self.lit_gene, self.biomarker, self.cyst_lineage):
+            self.score = 4
+            return True
+
+    def lvl2_rna(self):
         """{Lit} == {q75(Zscores)}"""
         if self.lit_gene == self.non_missing_zscore:
+            self.score = 3
+            return True
+
+    def lvl2_protein(self):
+        """{Lit} == {q75(Zscores)} or {q75(Zscores)} less than"""
+        if self.lit_gene == self.non_missing_zscore:
+            self.score = 3
+            return True
+        elif self.all_proceeds(self.lit_gene, self.zscore, self.germ_lineage):
+            self.score = 3
+            return True
+        elif self.all_proceeds(self.lit_gene, self.zscore, self.cyst_lineage):
             self.score = 3
             return True
 
@@ -74,22 +103,12 @@ class GeneValidator(object):
 
     def lvl3_protein(self):
         """Gene expression proceeds in the lineage."""
-        if (
-            self.lit_gene.issubset(self.germ_lineage)
-            & self.biomarker.issubset(self.germ_lineage)
-            & bool(self.biomarker)
-        ):
-            if self.proceeds(self.lit_gene, self.biomarker, self.germ_lineage):
-                self.score = 2
-                return True
-        elif (
-            self.lit_gene.issubset(self.cyst_lineage)
-            & self.biomarker.issubset(self.cyst_lineage)
-            & bool(self.biomarker)
-        ):
-            if self.proceeds(self.lit_gene, self.biomarker, self.cyst_lineage):
-                self.score = 2
-                return True
+        if self.any_proceeds(self.lit_gene, self.biomarker, self.germ_lineage):
+            self.score = 2
+            return True
+        elif self.any_proceeds(self.lit_gene, self.biomarker, self.cyst_lineage):
+            self.score = 2
+            return True
 
     def lvl4_rna(self):
         """({Lit} subset {Germ} & {q75(Zscores)} subset {Germ}) or
@@ -108,33 +127,71 @@ class GeneValidator(object):
             return True
 
     def lvl4_protein(self):
-        """Gene expression proceeds in the lineage."""
-        if (
-            self.lit_gene.issubset(self.germ_lineage)
-            & self.zscore.issubset(self.germ_lineage)
-            & bool(self.zscore)
-        ):
-            if self.proceeds(self.lit_gene, self.zscore, self.germ_lineage):
-                self.score = 1
-                return True
-        elif (
-            self.lit_gene.issubset(self.cyst_lineage)
-            & self.zscore.issubset(self.cyst_lineage)
-            & bool(self.zscore)
-        ):
-            protein_idx = max([self.cyst_lineage.index(x) + 1 for x in self.lit_gene])
-            if self.proceeds(self.lit_gene, self.zscore, self.cyst_lineage):
-                self.score = 1
-                return True
+        """Gene expression any_proceeds in the lineage."""
+        if self.any_proceeds(self.lit_gene, self.zscore, self.germ_lineage):
+            self.score = 1
+            return True
+        elif self.any_proceeds(self.lit_gene, self.zscore, self.cyst_lineage):
+            self.score = 1
+            return True
 
-    def proceeds(self, protein_cell_types, cell_types, lineage):
-        """Check if any cell types proceed the protein index."""
-        for protein, rna in product(protein_cell_types, cell_types):
+    def rna_proceeds_any(self, rna, proteins, lineage):
+        for protein in proteins:
             protein_idx = lineage.index(protein) + 1
             rna_idx = lineage.index(rna) + 1
-            _diff = protein_idx - rna_idx
-            if _diff in [0, 1]:
+            if rna_idx <= protein_idx:
                 return True
+        return False
+
+    def protein_proceeds_any(self, rnas, protein, lineage):
+        for rna in rnas:
+            protein_idx = lineage.index(protein) + 1
+            rna_idx = lineage.index(rna) + 1
+            if protein_idx <= rna_idx:
+                return True
+        return False
+
+    def all_proceeds(self, protein_cell_types, cell_types, lineage):
+        """Check if all cell types proceed or are the same as the protein index."""
+        protein_lineage = protein_cell_types.intersection(lineage)
+        rna_lineage = cell_types.intersection(lineage)
+
+        if (protein_lineage == set()) | (rna_lineage == set()):
+            return 
+
+        _rna_before_protein = np.array([
+            self.rna_proceeds_any(rna, protein_lineage, lineage)
+            for rna in rna_lineage
+        ])
+
+        _protein_before_rna = np.array([
+            self.protein_proceeds_any(rna_lineage, protein, lineage)
+            for protein in protein_lineage
+        ])
+
+        if np.all(_rna_before_protein) and not np.all(_protein_before_rna):
+            return True
+
+    def any_proceeds(self, protein_cell_types, cell_types, lineage):
+        """Check if any cell types proceed the protein index."""
+        protein_lineage = protein_cell_types.intersection(lineage)
+        rna_lineage = cell_types.intersection(lineage)
+
+        if (protein_lineage == set()) | (rna_lineage == set()):
+            return 
+
+        _rna_before_protein = np.array([
+            self.rna_proceeds_any(rna, protein_lineage, lineage)
+            for rna in rna_lineage
+        ])
+
+        _protein_before_rna = np.array([
+            self.protein_proceeds_any(rna_lineage, protein, lineage)
+            for protein in protein_lineage
+        ])
+
+        if np.any(_rna_before_protein):
+            return True
 
     def __str__(self):
         return f"{self.fbgn}\t{self.lit_gene}\t{self.biomarker}\t{self.zscore}\t{self.score}"
--- src/tests/test_validation.py
+++ src/tests/test_validation.py
@@ -16,7 +16,12 @@ from larval_gonad.validation import GeneValidator
         ({"G", "EPS"}, {"MPS", "EC"}, {"G", "EPS", "MPS"}, set(), False, 4),
         ## protein expression
         ({"G"}, set(), {"G"}, set(), True, 4),
+        ({"EPS"}, set(), {"G"}, set(), True, 4),
         ({"G", "EPS"}, set(), {"G", "EPS"}, set(), True, 4),
+        ({"EPS", "MPS", "LPS"}, set(), {"G", "EPS"}, set(), True, 4),
+        ({"C2"}, set(), {"C1"}, set(), True, 4),
+        ({"C1", "C2", "C3"}, set(), {"C1", "C2"}, set(), True, 4),
+
         # Threes: Lit Gene matches Upper Quantile of Zscores
         ## in situ
         ({"G"}, set(), set(), {"G"}, False, 3),
@@ -24,29 +29,35 @@ from larval_gonad.validation import GeneValidator
         ({"G", "EPS"}, {"MPS"}, set(), {"G", "EPS", "MPS"}, False, 3),
         ## protein expression
         ({"G"}, set(), set(), {"G"}, True, 3),
+        ({"EPS"}, set(), set(), {"G"}, True, 3),
         ({"G", "EPS"}, set(), set(), {"G", "EPS"}, True, 3),
+        ({"EPS", "MPS", "LPS"}, set(), set(), {"G", "EPS"}, True, 3),
+        ({"C2"}, set(), set(), {"C1"}, True, 3),
+        ({"C1", "C2", "C3"}, set(), set(), {"C1", "C2"}, True, 3),
+
         # Twos: Lit Gene matches Lineage Biomarkers
         ## in situ
         ({"G", "EPS"}, set(), {"G", "EPS", "MPS"}, set(), False, 2),
         ({"C1", "C4"}, set(), {"C1", "C2", "C3"}, set(), False, 2),
         ## protein expression
-        ({"EPS"}, set(), {"G"}, set(), True, 2),
-        ({"C2"}, set(), {"C1"}, set(), True, 2),
         ({"C1", "C2"}, set(), {"C1", "C2", "C3"}, set(), True, 2),
-        ({"EPS", "MPS", "LPS"}, set(), {"G", "EPS"}, set(), True, 2),
         ({"G", "EPS"}, set(), {"EPS", "MPS"}, set(), True, 2),
+        ({"G", "EPS", "MPS", "P"}, set(), {"EPS", "MPS"}, set(), True, 2),
+        ({"G", "EPS", "MPS"}, set(), {"EPS", "MPS", "P"}, set(), True, 2),
+
         # Ones: Lit Gene matches Lineage Zscores
         ## in situ
         ({"G", "EPS"}, set(), set(), {"G", "EPS", "MPS"}, False, 1),
         ({"C1", "C4"}, set(), set(), {"C1", "C2", "C3"}, False, 1),
         ## protein expression
         ({"G", "EPS"}, set(), set(), {"G", "EPS", "MPS"}, True, 1),
+
         # No class
+        (set(), {"G"}, set(), set(), False, None),
         ## in situ
         ({"G"}, set(), set(), set(), False, None),
         ## protein expression
         ({"G"}, set(), set(), set(), True, None),
-        ({"MPS"}, set(), {"G"}, set(), True, None),
     ],
 )
 def test_GeneValidator(lit, missing, biomarkers, zscores, flag_protein, expected_score):
