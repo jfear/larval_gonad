897fd39781fd7d3db4745ee2f3b707e5ae0af60c
Justin Fear <justin.m.fear@gmail.com>
2019-12-13 17:03:28 -0500

    Makes chrom expression panels with different scaling.

--- science_submission/scripts/plot_chrom_read_count_panel_scale_num_expressed_genes_per_scaffold.py
+++ science_submission/scripts/plot_chrom_read_count_panel_scale_num_expressed_genes_per_scaffold.py
@@ -10,8 +10,7 @@ figsize = (plt.rcParams["figure.figsize"][0], plt.rcParams["figure.figsize"][0])
 
 plot_defaults = dict(
     x="chrom",
-    y="Proportion Reads\n(Scaled by Chromosome Length)",
-    # y="Proportion Reads\n(Scaled by Number of Genes)",
+    y="Proportion Reads\n(Scaled by Number of Expressed Genes)",
     order=["X", "2L", "2R", "3L", "3R", "4", "Y"],
     capsize=0.1,
 )
@@ -25,9 +24,7 @@ def main():
     plot_adult_bulk(axes[0, 1], df)
     plot_L3_scAgg(axes[1, 0], df)
     plot_L3_scLineage(axes[1, 1], df)
-    # plot_adult_scAgg(axes[1, 1], df)
-    # plot_adult_scLineage(axes[2, 1], df)
-    axes[0, 0].set(ylim=(0, 0.12), yticks=[0, 0.06, 0.12])
+    axes[0, 0].set(ylim=(0, 0.15), yticks=[0, 0.06, 0.12])
 
     for ax in axes[:, 1]:
         ax.set(ylabel="")
@@ -42,7 +39,14 @@ def plot_L3_bulk(ax, df):
     testis = snakemake.params.colors["testis"][0]
     ovary = snakemake.params.colors["ovary"][0]
     dat = df.query("stage == 'L3' and data_source == 'RNA-Seq'")
-    sns.barplot(data=dat, ax=ax, hue="tissue", hue_order=["testis", "ovary"], palette=[testis, ovary], **plot_defaults)
+    sns.barplot(
+        data=dat,
+        ax=ax,
+        hue="tissue",
+        hue_order=["testis", "ovary"],
+        palette=[testis, ovary],
+        **plot_defaults
+    )
     ax.set(title="L3 Bulk", xlabel="")
     ax.get_legend().remove()
     return ax
@@ -52,7 +56,14 @@ def plot_adult_bulk(ax, df):
     testis = snakemake.params.colors["testis"][0]
     ovary = snakemake.params.colors["ovary"][0]
     dat = df.query("stage == 'adult' and data_source == 'RNA-Seq'")
-    sns.barplot(data=dat, ax=ax, hue="tissue", hue_order=["testis", "ovary"], palette=[testis, ovary], **plot_defaults)
+    sns.barplot(
+        data=dat,
+        ax=ax,
+        hue="tissue",
+        hue_order=["testis", "ovary"],
+        palette=[testis, ovary],
+        **plot_defaults
+    )
     ax.set(title="Adult Bulk", xlabel="")
     ax.legend(loc="upper left", bbox_to_anchor=[1, 1])
     return ax
@@ -63,14 +74,6 @@ def plot_L3_scAgg(ax, df):
     dat = df.query("stage == 'L3' and data_source == 'scRNA-Seq' and cell_type == 'None'")
     sns.barplot(data=dat, ax=ax, palette=[testis], **plot_defaults)
     ax.set(title="L3 scRNA-Seq", xlabel="")
-    return ax
-
-
-def plot_adult_scAgg(ax, df):
-    testis = snakemake.params.colors["testis"][0]
-    dat = df.query("stage == 'adult' and data_source == 'scRNA-Seq' and cell_type == 'None'")
-    sns.barplot(data=dat, ax=ax, palette=[testis], **plot_defaults)
-    ax.set(title="Adult scRNA-Seq", xlabel="")
     return ax
 
 
@@ -93,36 +96,21 @@ def plot_L3_scLineage(ax, df):
     return ax
 
 
-def plot_adult_scLineage(ax, df):
-    dat = df.query("stage == 'adult' and data_source == 'scRNA-Seq' and cell_type != 'None'")
-    sns.barplot(
-        data=dat,
-        ax=ax,
-        hue="cell_type",
-        hue_order=["Germline", "Cyst Lineage", "Other Somatic"],
-        **plot_defaults
-    )
-    ax.set(title="Adult scRNA-Seq Lineages", xlabel="")
-    ax.legend(loc="upper left", bbox_to_anchor=[1, 1])
-    return ax
-
-
 def munge():
-    # fbgn2chrom = get_fbgn2chrom().value_counts().rename("num_genes")
-    # fbgn2chrom.index.name = "chrom"
-    chrom_sizes = get_chromsizes()
-
     counts = pd.concat(
         [
             pd.read_feather(snakemake.input.adult_bulk),
             pd.read_feather(snakemake.input.L3_bulk),
             pd.read_feather(snakemake.input.L3_sc_agg),
             pd.read_feather(snakemake.input.L3_sc_by_class),
-            # pd.read_feather(snakemake.input.adult_sc_agg),
-            # pd.read_feather(snakemake.input.adult_sc_by_class),
         ],
         sort=False,
     ).fillna("None")
+
+    expressed = counts.loc[counts.Count > 0, "FBgn"].unique().tolist()
+
+    fbgn2chrom = get_fbgn2chrom(expressed).value_counts().rename("num_genes")
+    fbgn2chrom.index.name = "chrom"
 
     total_counts = counts.groupby(
         ["stage", "data_source", "cell_type", "rep", "tissue"]
@@ -132,30 +120,21 @@ def munge():
         counts.groupby(["chrom", "stage", "data_source", "cell_type", "rep", "tissue"])
         .Count.sum()
         .div(total_counts)
-        .div(chrom_sizes / 1e7, axis=0)
-        # .div(fbgn2chrom / 1e3, axis=0)
+        .div(fbgn2chrom / 1e3, axis=0)
         .rename(plot_defaults["y"])
     )
 
     return prop_counts.to_frame().reset_index()
 
 
-def get_fbgn2chrom():
+def get_fbgn2chrom(fbgns):
     return (
         pd.read_feather(snakemake.input.gene_annot, columns=["FBgn", "FB_chrom"])
         .set_index("FBgn")
         .query('FB_chrom in ["X", "2L", "2R", "3L", "3R", "4", "Y"]')
         .squeeze()
         .rename("chrom")
-    )
-
-
-def get_chromsizes():
-    return (
-        pd.read_csv(snakemake.input.chrom_sizes, sep="\t", header=None, names=["chrom", "length"])
-        .assign(chrom=lambda x: x.chrom.str.replace("chr", ""))
-        .set_index("chrom")
-        .squeeze()
+        .reindex(fbgns)
     )
 
 
@@ -163,21 +142,19 @@ if __name__ == "__main__":
     if os.getenv("SNAKE_DEBUG", False):
         from larval_gonad.debug import snakemake_debug
         from larval_gonad.config import read_config
+
         COLORS = read_config("config/colors.yaml")
 
         snakemake = snakemake_debug(
             workdir="science_submission",
             input=dict(
-                # gene_annot="../references/gene_annotation_dmel_r6-26.feather",
-                chrom_sizes="../lcdb-references/dmel/r6-26/fasta/dmel_r6-26.chromsizes",
+                gene_annot="../references/gene_annotation_dmel_r6-26.feather",
                 adult_bulk="../output/expression-atlas-wf/w1118_gene_counts.feather",
                 L3_bulk="../output/bulk2-rnaseq-wf/testis_ovary_counts.feather",
                 L3_sc_agg="../output/seurat3-cluster-wf/aggegated_gene_counts.feather",
                 L3_sc_by_class="../output/seurat3-cluster-wf/aggegated_gene_counts_by_germ_soma.feather",
-                # adult_sc_agg="",
-                # adult_sc_by_class="",
             ),
-            params=dict(colors=COLORS)
+            params=dict(colors=COLORS),
         )
 
     main()
