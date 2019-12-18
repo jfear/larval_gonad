f1cd05e1219c3922fea97b6d8ddce3e791d5e58a
Justin Fear <justin.m.fear@gmail.com>
2019-12-18 15:40:04 -0500

    Figuring out annotation.

--- adult-testis-scRNAseq-wf/scripts/annotate_clusters.py
+++ adult-testis-scRNAseq-wf/scripts/annotate_clusters.py
@@ -5,19 +5,34 @@ adult annotations. I am using a simple correlation metric.
 
 """
 import os
-import pandas as pd
 
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
+from more_itertools import flatten
 
+from larval_gonad.config import read_config
 from larval_gonad.normalization import tpm
 
 
 def main():
+    corr = plot_corr()
+
+    lit_genes = list(flatten(read_config(snakemake.input.lit_genes).values()))
+    biomarkers = pd.read_feather(snakemake.input.adult_biomarkers).query(
+        f"FBgn == {lit_genes} & p_val_adj <= 0.01"
+    )
+
+    biomarkers.groupby(["FBgn", "gene_symbol"]).apply(
+        lambda x: "|".join(x.cluster.sort_values())
+    ).rename("clusters").reset_index().set_index("FBgn").reindex(lit_genes).dropna()
+
+
+def plot_corr():
     adult = munge_data(snakemake.input.adult_metadata, snakemake.input.adult)
     larval = munge_data(snakemake.input.larval_metadata, snakemake.input.larval)
     corr = pd.merge(normalize(adult), normalize(larval), on="FBgn").corr()
-    sns.clustermap(corr)
+    return sns.clustermap(corr)
 
 
 def munge_data(metadata, data):
@@ -55,8 +70,10 @@ if __name__ == "__main__":
             workdir="adult-testis-scRNAseq-wf",
             input=dict(
                 gene_annot="../references/gene_annotation_dmel_r6-26.feather",
+                lit_genes="../config/literature_genes.yaml",
                 adult="../output/adult-testis-scRNAseq-wf/raw_Ral517.feather",
                 adult_metadata="../output/adult-testis-scRNAseq-wf/Ral517_metadata.feather",
+                adult_biomarkers="../output/adult-testis-scRNAseq-wf/Ral517_biomarkers.feather",
                 larval="../output/cellselection-wf/raw.feather",
                 larval_metadata="../output/seurat3-cluster-wf/combined_n3_clusters.feather",
             ),
