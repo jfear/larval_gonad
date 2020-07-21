5ceb38de1d427836893812c85650d125044a9e6f
Justin Fear <justin.m.fear@gmail.com>
2020-07-21 12:55:56 -0400

    Plots total expression for different gene sets by cell-type

--- paper_submission/debug/stats_gene_sets_total_expression_by_cell_type.py
+++ paper_submission/debug/stats_gene_sets_total_expression_by_cell_type.py
@@ -4,11 +4,9 @@ from larval_gonad.mock import MockSnake
 from larval_gonad.config import read_config
 
 config = read_config("../../config/common.yaml")
-color_config = read_config("../../config/colors.yaml")
 
 builtins.snakemake = MockSnake(
     input=dict(
-        gene_annot="../../references/gene_annotation_dmel_r6-26.feather",
         tpm="../../output/seurat3-cluster-wf/tpm_by_cluster.feather",
         expressed_fbgns="../../output/cellselection-wf/expressed_genes.pkl",
         widely_expressed_fbgns="../../output/cellselection-wf/commonly_expressed_genes.pkl",
@@ -16,8 +14,6 @@ builtins.snakemake = MockSnake(
         tsps_fbgns="../../output/expression-atlas-wf/dmel_male_tsps_fbgns.pkl",
     ),
     params=dict(
-        colors=color_config["clusters"],
-        order=config["cluster_order"],
-        names=config["cluster_names"],
-    ),
+        order=config["cluster_order"]
+    )
 )
---
+++ paper_submission/scripts/stats_gene_sets_total_expression_by_cell_type.py
@@ -0,0 +1,48 @@
+"""Plot total expression (TPM) for different gene sets."""
+import joblib
+import numpy as np
+import pandas as pd
+
+from larval_gonad.stats import PairwisePermutationTest
+
+THREADS = snakemake.threads
+
+
+def main():
+    tpm = pd.read_feather(snakemake.input.tpm)
+    df = pd.concat(
+        [
+            create_gene_subset(tpm, snakemake.input.expressed_fbgns, "All Expressed"),
+            create_gene_subset(
+                tpm, snakemake.input.widely_expressed_fbgns, "Widely Expressed"
+            ),
+            create_gene_subset(tpm, snakemake.input.tau_fbgns, "Tau"),
+            create_gene_subset(tpm, snakemake.input.tsps_fbgns, "TSPS"),
+        ],
+        ignore_index=True,
+    )
+
+    df.groupby("gene_set").apply(run_stats).to_csv(snakemake.output[0], sep="\t")
+
+
+def create_gene_subset(df: pd.DataFrame, gene_set: str, name: str):
+    fbgns = joblib.load(gene_set)  # pylint: disable=unused-variable
+    return df.query("FBgn in @fbgns").assign(gene_set=name)
+
+
+def run_stats(df: pd.DataFrame) -> pd.DataFrame:
+    pw = PairwisePermutationTest("cluster", "TPM", data=df, threads=THREADS).fit()
+    return (
+        pw.results.assign(
+            name1=lambda x: pd.Categorical(x.name1, categories=snakemake.params.order)
+        )
+        .assign(
+            name2=lambda x: pd.Categorical(x.name2, categories=snakemake.params.order)
+        )
+        .sort_values(["name1", "name2"])
+        .set_index(["name1", "name2"])
+    )
+
+
+if __name__ == "__main__":
+    main()
