84c972e54cc4343c204601d6d34f079648318aef
Justin Fear <justin.m.fear@gmail.com>
2020-07-21 14:00:40 -0400

    Re-order table output.

--- paper_submission/debug/stats_gsea_pairwise_comparisons_of_cell_types.py
+++ paper_submission/debug/stats_gsea_pairwise_comparisons_of_cell_types.py
@@ -1,7 +1,11 @@
 import builtins
 
 from larval_gonad.mock import MockSnake
+from larval_gonad.config import read_config
+
+config = read_config("../../config/common.yaml")
 
 builtins.snakemake = MockSnake(
     input="../../output/paper_submission/gsea_by_cell_type.feather",
+    params=dict(order=config["cluster_order"]),
 )
--- paper_submission/scripts/stats_gsea_pairwise_comparisons_of_cell_types.py
+++ paper_submission/scripts/stats_gsea_pairwise_comparisons_of_cell_types.py
@@ -10,7 +10,19 @@ def main():
     df = pd.read_feather(snakemake.input[0])
     pw = PairwisePermutationTest("cluster", "male", data=df, threads=THREADS)
     pw.fit()
-    pw.results.to_csv(snakemake.output[0], sep="\t", index=False)
+
+    (
+        pw.results
+        .assign(
+            name1=lambda x: pd.Categorical(x.name1, categories=snakemake.params.order)
+        )
+        .assign(
+            name2=lambda x: pd.Categorical(x.name2, categories=snakemake.params.order)
+        )
+        .sort_values(["name1", "name2"])
+        .set_index(["name1", "name2"])
+        .to_csv(snakemake.output[0], sep="\t")
+    )
 
 
 
