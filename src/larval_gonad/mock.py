c4057796cb7bdbc63edef504025f634538a7f70b
Justin Fear <justin.m.fear@gmail.com>
2020-07-22 17:13:21 -0400

    Adds threads to mock.

--- src/larval_gonad/mock.py
+++ src/larval_gonad/mock.py
@@ -4,20 +4,32 @@ from snakemake.io import Namedlist, InputFiles, OutputFiles, Wildcards, Params, 
 class MockSnake:
     """Mock the snakemake class used inside of python scripts."""
 
-    def __init__(self, input=None, output=None, params=None, wildcards=None, log="", config=None):
+    def __init__(
+        self,
+        input=None,
+        output=None,
+        params=None,
+        wildcards=None,
+        log="",
+        config=None,
+        threads=None,
+    ):
         self.input = InputFiles(self.make_namedlist(input))
         self.output = OutputFiles(self.make_namedlist(output))
         self.params = Params(self.make_namedlist(params))
         self.wildcards = Wildcards(self.make_namedlist(wildcards))
         self.log = Log(log)
         self.config = config or {}
+        self.threads = threads or 1
         self.rulename = "mock"
 
     def make_namedlist(self, item, first_lvl=True):
         if isinstance(item, list):
             return Namedlist(item)
         elif isinstance(item, dict):
-            return Namedlist(fromdict={k: self.make_namedlist(v, False) for k, v in item.items()})
+            return Namedlist(
+                fromdict={k: self.make_namedlist(v, False) for k, v in item.items()}
+            )
         elif isinstance(item, (str, int, float)):
             if first_lvl:
                 return Namedlist([item])
